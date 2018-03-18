f356ecca23fa14f529fa3f7fd214834d57b6292c
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-03-18 13:48:44 +0100

    Made the data be displayed in the hierarchical form.

---
+++ src/components/DataRow.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import DataList from './DataList';
+
+class DataRow extends Component {
+    render() {
+        /* Name of the ID property. */
+        const idPropertyName = 'id';
+
+        /* Name of the property that should be displayed as text. */
+        const displayPropertyName = 'value';
+
+        /* Hidden properties. */
+        const ignoredProperties = [idPropertyName, displayPropertyName];
+
+        const data = this.props.data;
+
+        if (!data) {
+            return null;
+        }
+
+        if (data instanceof Object) {
+            /* Objects should be rendered with their content. */
+            const rows = [];
+
+            const propertiesContent =  Object.entries(data).filter(
+                (entry) => !(entry[0] in ignoredProperties) && entry[1] instanceof Array
+            );
+
+            if (propertiesContent.length > 0) {
+                propertiesContent.forEach(
+                    (value, index) => {
+                        console.log(value + ', ' + index);
+                        rows.push(
+                            <span>
+                                {index == 0 ? <br/> : null}
+                                {value[0]}<br/>
+                                <DataList key={index} data={value[1]}/><br/>
+                            </span>
+                        );
+                    }
+                );
+            }
+
+            return (
+                <li>
+                    {data[displayPropertyName]}
+                    <br/>
+                    {rows}
+                </li>
+            );
+        } else {
+            /* Non objects should be rendered as is. */
+            return (
+                <li>
+                    {data}
+                </li>
+            );
+        }
+    }
+}
+
+export default DataRow;
