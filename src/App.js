5a893e70a87d0a00d11202ce63e421cac892cd4c
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-03-27 13:47:58 +0200

    Added visjs example graph.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import DataList from './components/DataList';
+import Graph from 'vis-react';
 
 class App extends Component {
     constructor(props) {
@@ -61,8 +62,40 @@ class App extends Component {
             return (<p><strong>Error:</strong> {this.props.error} </p>);
         }
 
+        var graph = {
+          nodes: [
+              {id: 1, label: 'Node 1'},
+              {id: 2, label: 'Node 2'},
+              {id: 3, label: 'Node 3'},
+              {id: 4, label: 'Node 4'},
+              {id: 5, label: 'Node 5'}
+            ],
+          edges: [
+              {from: 1, to: 2},
+              {from: 1, to: 3},
+              {from: 2, to: 4},
+              {from: 2, to: 5}
+            ]
+        };
+
+        var options = {
+            layout: {
+                hierarchical: true
+            },
+            edges: {
+                color: "#000000"
+            }
+        };
+
+        var events = {
+            select: function(event) {
+                var { nodes, edges } = event;
+            }
+        }
+
         return (
             <div className="App">
+                <Graph graph={graph} options={options} events={events}/>
                 <header className="App-header">
                     <h1 className="App-title">Research contribution</h1>
                 </header>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import ErrorBoundary from './ErrorBoundary';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
