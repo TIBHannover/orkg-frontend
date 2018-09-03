6530afe39a84b3be615ecb06e73ad173bfcdc614
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-03 14:46:17 +0200

    Made the structure represent that of Wikidata more.

--- src/components/statements/Statement.js
+++ src/components/statements/Statement.js
@@ -5,9 +5,17 @@ export default class Statement extends Component {
     render() {
         return <div className="statementView">
             <div className="statementView-rankSelector"/>
-            <div className="statementView-mainPiece-container">
-                <div className="statementView-mainPiece">
-                    {this.props.children}
+            <div className="statementView-mainSnak-container">
+                <div className="statementView-mainSnak">
+                    <div className="snakView-value-container">
+                        <div className="snakView-typeSelector"/>
+                        <div className="snakView-body">
+                            <div className="snakView-value">
+                                {this.props.children}
+                            </div>
+                            <div className="snakView-indicators"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
