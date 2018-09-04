6e4500edf3aae8c3a6c70ee19ae13194ed257009
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-04 12:13:31 +0200

    Changed the structure of data to represent the orkg-core.owl ontology. Expanding the ontology.

--- src/App.js
+++ src/App.js
@@ -286,101 +286,102 @@ class App extends Component {
 
                         Average-case optimal divide and conquer comparison sorting algorithm
 
-                        <h2 className="h3">Statements</h2>
-
                         <StatementsCard href="#" label="instance of">
+                            <Statement><a href="#">Research contribution</a></Statement>
                             <Statement><a href="#">Comparison sort</a></Statement>
-                            <Statement><a href="#">Divide and conquer algorithm</a></Statement>
                         </StatementsCard>
 
-                        <StatementsCard href="#" label="image">
-                            <Statement>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Sorting_quicksort_anim.gif"/>
-                            </Statement>
+                        <StatementsCard href="#" label="addresses">
+                            <Statement><a href="#">Array sorting</a></Statement>
                         </StatementsCard>
 
-                        <StatementsCard href="#" label="discoverer or inventor">
-                            <Statement>
-                                <a href="#">Charles Antony Richard Hoare</a>
-                            </Statement>
+                        <StatementsCard href="#" label="employs">
+                            <Statement><a href="#">Quick sort method</a></Statement>
+                            <Statement><a href="#">Quick sort formal analysis</a></Statement>
+                            <Statement><a href="#">Lomuto partition scheme</a></Statement>
+                            <Statement><a href="#">Hoare partition scheme</a></Statement>
                         </StatementsCard>
 
-                        <StatementsCard href="#" label="worst-case performance">
-                            <Statement>
-                                n^2
-                            </Statement>
+                        <StatementsCard href="#" label="yields">
+                            <Statement/>
                         </StatementsCard>
 
-                        <StatementsCard href="#" label="best-case performance">
-                            <Statement>
-                                n log(n)
-                            </Statement>
-                        </StatementsCard>
+                        {/*<StatementsCard href="#" label="worst-case performance">*/}
+                            {/*<Statement>*/}
+                                {/*n^2*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
 
-                        <StatementsCard href="#" label="average performance">
-                            <Statement>
-                                n log(n)
-                            </Statement>
-                        </StatementsCard>
+                        {/*<StatementsCard href="#" label="best-case performance">*/}
+                            {/*<Statement>*/}
+                                {/*n log(n)*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
 
-                        <StatementsCard href="#" label="worst-case space complexity">
-                            <Statement>
-                                n
-                            </Statement>
-                        </StatementsCard>
+                        {/*<StatementsCard href="#" label="average performance">*/}
+                            {/*<Statement>*/}
+                                {/*n log(n)*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
 
-                        <StatementsCard href="#" label="average space complexity">
-                            <Statement>
-                                log(n)
-                            </Statement>
-                        </StatementsCard>
+                        {/*<StatementsCard href="#" label="worst-case space complexity">*/}
+                            {/*<Statement>*/}
+                                {/*n*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
 
-                        <StatementsCard href="#" label="code">
-                            <Statement>
-                                <CodeContainer>
-{`algorithm quicksort(A, lo, hi) is
-    if lo < hi then
-        p := partition(A, lo, hi)
-        quicksort(A, lo, p - 1 )
-        quicksort(A, p + 1, hi)
+                        {/*<StatementsCard href="#" label="average space complexity">*/}
+                            {/*<Statement>*/}
+                                {/*log(n)*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
 
-algorithm partition(A, lo, hi) is
-    pivot := A[hi]
-    i := lo
-    for j := lo to hi - 1 do
-        if A[j] < pivot then
-            swap A[i] with A[j]
-            i := i + 1
-    swap A[i] with A[hi]
-    return i`}
-                                </CodeContainer>
-                                <CodeContainer>
-{`algorithm quicksort(A, lo, hi) is
-    if lo < hi then
-        p := partition(A, lo, hi)
-        quicksort(A, lo, p)
-        quicksort(A, p + 1, hi)
+                        {/*<StatementsCard href="#" label="code">*/}
+                            {/*<Statement>*/}
+                                {/*<CodeContainer>*/}
+{/*{`algorithm quicksort(A, lo, hi) is*/}
+    {/*if lo < hi then*/}
+        {/*p := partition(A, lo, hi)*/}
+        {/*quicksort(A, lo, p - 1 )*/}
+        {/*quicksort(A, p + 1, hi)*/}
 
-algorithm partition(A, lo, hi) is
-    pivot := A[lo]
-    i := lo - 1
-    j := hi + 1
-    loop forever
-        do
-            i := i + 1
-        while A[i] < pivot
+{/*algorithm partition(A, lo, hi) is*/}
+    {/*pivot := A[hi]*/}
+    {/*i := lo*/}
+    {/*for j := lo to hi - 1 do*/}
+        {/*if A[j] < pivot then*/}
+            {/*swap A[i] with A[j]*/}
+            {/*i := i + 1*/}
+    {/*swap A[i] with A[hi]*/}
+    {/*return i`}*/}
+                                {/*</CodeContainer>*/}
+                                {/*<CodeContainer>*/}
+{/*{`algorithm quicksort(A, lo, hi) is*/}
+    {/*if lo < hi then*/}
+        {/*p := partition(A, lo, hi)*/}
+        {/*quicksort(A, lo, p)*/}
+        {/*quicksort(A, p + 1, hi)*/}
 
-        do
-            j := j - 1
-        while A[j] > pivot
+{/*algorithm partition(A, lo, hi) is*/}
+    {/*pivot := A[lo]*/}
+    {/*i := lo - 1*/}
+    {/*j := hi + 1*/}
+    {/*loop forever*/}
+        {/*do*/}
+            {/*i := i + 1*/}
+        {/*while A[i] < pivot*/}
 
-        if i >= j then
-            return j
+        {/*do*/}
+            {/*j := j - 1*/}
+        {/*while A[j] > pivot*/}
 
-        swap A[i] with A[j]`}
-                                </CodeContainer>
-                            </Statement>
-                        </StatementsCard>
+        {/*if i >= j then*/}
+            {/*return j*/}
+
+        {/*swap A[i] with A[j]`}*/}
+                                {/*</CodeContainer>*/}
+                            {/*</Statement>*/}
+                        {/*</StatementsCard>*/}
                     </main>
                 </div>
             </div>
