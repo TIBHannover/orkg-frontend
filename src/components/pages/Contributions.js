75b9b1406f98bde76f89189710bdabbd8c7b3a22
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-10 16:57:45 +0200

    Added special component to display records.

--- src/components/pages/Contributions.js
+++ src/components/pages/Contributions.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ShortRecord from "../statements/ShortRecord";
 
 export default class Contributions extends Component {
 
@@ -7,7 +8,20 @@ export default class Contributions extends Component {
     }
 
     render() {
-        return <div>Contributions</div>;
+        return <div>
+            <ShortRecord header="http://orkg.tib.eu/resource/68bcfb497a403353f68bf3144b35aaf5">
+                ""
+            </ShortRecord>
+            <ShortRecord header="http://orkg.tib.eu/resource/0">
+                "Record 1"
+            </ShortRecord>
+            <ShortRecord header="http://orkg.tib.eu/resource/1">
+                "Record 2"
+            </ShortRecord>
+            <ShortRecord header="http://orkg.tib.eu/resource/2">
+                "Record 3"
+            </ShortRecord>
+        </div>;
     }
 
 }
