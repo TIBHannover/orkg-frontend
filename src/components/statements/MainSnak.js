4a551b591fd8bb58fd8291dbd37848bd9341f0c5
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-15 17:15:54 +0200

    Extracted the information and editing part to MainSnak. Added textarea for editing.

---
+++ src/components/statements/MainSnak.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+export default class EditToolbar extends Component {
+
+    render() {
+        let content = null;
+        if (!this.props.editing) {
+            content = <div className="snakView">
+                <div className="snakView-value-container">
+                    <div className="snakView-typeSelector"/>
+                    <div className="snakView-body">
+                        <div className="snakView-value">
+                            {this.props.children}
+                        </div>
+                        <div className="snakView-indicators"/>
+                    </div>
+                </div>
+            </div>
+        } else {
+            content = <div className="snakView edit" aria-disabled="false">
+                {/*<div className="snakView-property-container">*/}
+                    {/*<div className="snakView-property" dir="auto"></div>*/}
+                {/*</div>*/}
+                <div className="snakView-value-container" dir="auto">
+                    <div className="snakView-body">
+                        <div className="snakView-value snakView-variation-valueSnak ">
+                            <div className="valueView valueView-inEditMode">
+                                <div className="valueView-value">
+                                    <textarea className="valueView-input"
+                                            style={{height: "20.7812px", overflow: "hidden", resize: "none"}}>
+                                    </textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="wikibase-snakview-indicators"></div>
+                    </div>
+                </div>
+            </div>
+        }
+
+        return <div className="statementView-mainSnak">
+            {content}
+        </div>
+    }
+
+}
--- src/components/statements/Statement.js
+++ src/components/statements/Statement.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import EditToolbar from "./EditToolbar";
+import MainSnak from "./MainSnak";
 
 export default class Statement extends Component {
 
@@ -7,19 +8,9 @@ export default class Statement extends Component {
         return <div className="statementView">
             <div className="statementView-rankSelector"/>
             <div className="statementView-mainSnak-container">
-                <div className="statementView-mainSnak">
-                    <div className="snakView">
-                        <div className="snakView-value-container">
-                            <div className="snakView-typeSelector"/>
-                            <div className="snakView-body">
-                                <div className="snakView-value">
-                                    {this.props.children}
-                                </div>
-                                <div className="snakView-indicators"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <MainSnak editing={true}>
+                    {this.props.children}
+                </MainSnak>
             </div>
             <span className="editToolbar-container toolbar-container" aria-disabled={false}>
                 <EditToolbar editing={true}/>
