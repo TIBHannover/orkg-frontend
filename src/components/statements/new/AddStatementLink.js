2a7570281ae288fe607634b5f162447aa9893ed5
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-09-21 13:14:16 +0200

    Made the new statement card appear when 'add statement' is clicked.

--- src/components/statements/new/AddStatementLink.js
+++ src/components/statements/new/AddStatementLink.js
@@ -5,7 +5,7 @@ export default class AddStatementLink extends Component {
     render() {
         return <div className="addToolbar toolbar addToolbar-container toolbar-container">
             <span className="toolbar-button toolbar-button-add">
-                <a href="javascript:void(0)" title="Add a new statement">
+                <a href="javascript:void(0)" title="Add a new statement" onClick={this.props.onClick}>
                     <span className="fa fa-plus"/>add statement
                 </a>
             </span>
--- src/components/statements/new/NewStatementsSection.js
+++ src/components/statements/new/NewStatementsSection.js
@@ -4,15 +4,22 @@ import AddStatementLink from './AddStatementLink';
 
 export default class NewStatementsSection extends Component {
 
+    state = {
+        newStatementBoxes: [],
+    };
+
+    onAddNewStatement = () => {
+        this.state.newStatementBoxes.push(<NewStatementGroupCard onUpdate={this.reset}
+                getStatementText={this.getStatementText} setStatementText={this.setStatementText}/>);
+        this.forceUpdate();
+    };
+
     render () {
         const newStatementJsx = <NewStatementGroupCard onUpdate={this.reset} getStatementText={this.getStatementText}
                 setStatementText={this.setStatementText}/>;
-        const addStatementLinkJsx = <AddStatementLink/>;
+        const addStatementLinkJsx = <AddStatementLink onClick={this.onAddNewStatement}/>;
 
-        return [
-            newStatementJsx,
-            addStatementLinkJsx
-        ];
+        return this.state.newStatementBoxes.concat([addStatementLinkJsx]);
     }
 
 }
