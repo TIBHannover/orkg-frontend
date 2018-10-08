c7441fe2494bb455314e6c6970f26ee34cedb94b
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-10-08 14:53:44 +0200

    Made the object input appear when the user selects a property and disappear, when the user modifies the text of the selected property.

--- src/components/statements/MainSnak.js
+++ src/components/statements/MainSnak.js
@@ -4,8 +4,16 @@ import EditableDropdown from './new/EditableDropdown';
 
 export default class MainSnak extends Component {
 
+    state = {
+        selectedPropertyId: null,
+    };
+
     handleItemSelect = (itemName) => {
         this.props.onObjectTypeSelect(itemName);
+    };
+
+    handlePropertySelect = (propertyId) => {
+        this.setState({selectedPropertyId: propertyId});
     };
 
     render() {
@@ -35,33 +43,37 @@ export default class MainSnak extends Component {
                 {
                     this.props.newProperty && <div className="snakView-property-container">
                         <div className="snakView-property" dir="auto">
-                            {/*<input placeholder="property" style={inputStyle}/>*/}
-
-                            <EditableDropdown/>
+                            <EditableDropdown onItemSelected={this.handlePropertySelect}/>
                         </div>
                     </div>
                 }
-                <div className="snakView-value-container" dir="auto">
-                    <ObjectTypeSelector onItemSelect={this.handleItemSelect} objectType={this.props.objectType}/>
-                    {shouldShowQuotes && <div className="valueView-input-group-prepend">
-                        <span className="valueView-input-group-text">&quot;</span>
-                    </div>}
-                    <div className="snakView-body">
-                        <div className="snakView-value snakView-variation-valueSnak ">
-                            <div className="valueView valueView-inEditMode">
-                                <div className="valueView-value">
-                                    <textarea className="valueView-input" defaultValue={this.props.value}
-                                            style={inputStyle} onInput={this.props.onInput}>
-                                    </textarea>
+                {
+                    !this.props.newProperty || this.state.selectedPropertyId != null ?
+                    <div className="snakView-value-container" dir="auto">
+                        <ObjectTypeSelector onItemSelect={this.handleItemSelect} objectType={this.props.objectType}/>
+                        {shouldShowQuotes && <div className="valueView-input-group-prepend">
+                            <span className="valueView-input-group-text">&quot;</span>
+                        </div>}
+
+                        <div className="snakView-body">
+                            <div className="snakView-value snakView-variation-valueSnak ">
+                                <div className="valueView valueView-inEditMode">
+                                    <div className="valueView-value">
+                                        <textarea className="valueView-input" defaultValue={this.props.value}
+                                                  style={inputStyle} onInput={this.props.onInput}
+                                                  autoFocus={this.state.selectedPropertyId != null}>
+                                        </textarea>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="snakView-indicators"></div>
                         </div>
-                        <div className="snakView-indicators"></div>
+                        {shouldShowQuotes && <div className="valueView-input-group-prepend">
+                            <span className="valueView-input-group-text">&quot;</span>
+                        </div>}
                     </div>
-                    {shouldShowQuotes && <div className="valueView-input-group-prepend">
-                        <span className="valueView-input-group-text">&quot;</span>
-                    </div>}
-                </div>
+                    : null
+                }
             </div>
         }
 
--- src/components/statements/new/EditableDropdown.js
+++ src/components/statements/new/EditableDropdown.js
@@ -5,6 +5,7 @@ import {getPredicatesByLabel} from '../../../helpers';
 export default class EditableDropdown extends Component {
     state = {
         value: '',
+        selectedItemId: null,
         dropdownMenuJsx: null,
     };
 
@@ -12,13 +13,20 @@ export default class EditableDropdown extends Component {
     handleChange = (event) => {
         const maxValues = 10;
         const value = event.target.value;
-        this.setState({value: value});
+
+        if (this.state.selectedItemId) {
+            this.props.onItemSelected(null);
+        }
+        this.setState({
+            value: value,
+            selectedItemId: null,
+        });
 
         if (value && value.length >= 0) {
             getPredicatesByLabel(value, (responseJson) => {
                     if (responseJson.length > 0) {
                         const menuItemsJsx = responseJson.map(
-                            (predicate) => <a id={predicate.id} className="dropdown-item" href="#"
+                            (predicate) => <a id={predicate.id} key={predicate.id} className="dropdown-item" href="#"
                                     onClick={this.handleItemClick}>{predicate.label}</a>).slice(0, maxValues);
                         this.setState({
                             dropdownMenuJsx: <div className="dropdown-menu">{menuItemsJsx}</div>,
@@ -36,8 +44,12 @@ export default class EditableDropdown extends Component {
     };
 
     handleItemClick = (event) => {
-        this.setState({value: event.target.innerText});
+        this.setState({
+            value: event.target.innerText,
+            selectedItemId: event.target.id,
+        });
         this.hideDropdownMenu();
+        this.props.onItemSelected(event.target.id);
     };
 
     hideDropdownMenu = () => {
