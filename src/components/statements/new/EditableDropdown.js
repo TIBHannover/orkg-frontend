e5d5c5ee744c556b82c4ac9854e0db5ddab5641b
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-10-08 13:34:58 +0200

    Small corrections to the predicate dropdown editable box when adding a new statement with predicate.

--- src/components/statements/new/EditableDropdown.js
+++ src/components/statements/new/EditableDropdown.js
@@ -2,7 +2,6 @@ import {Component} from 'react';
 import React from 'react';
 import {getPredicatesByLabel} from '../../../helpers';
 
-
 export default class EditableDropdown extends Component {
     state = {
         value: '',
@@ -11,6 +10,7 @@ export default class EditableDropdown extends Component {
 
     // TODO: add timer, so that the request is not sent on every keystroke.
     handleChange = (event) => {
+        const maxValues = 10;
         const value = event.target.value;
         this.setState({value: value});
 
@@ -19,24 +19,29 @@ export default class EditableDropdown extends Component {
                     if (responseJson.length > 0) {
                         const menuItemsJsx = responseJson.map(
                             (predicate) => <a id={predicate.id} className="dropdown-item" href="#"
-                                    onClick={this.handleItemClick}>{predicate.label}</a>);
+                                    onClick={this.handleItemClick}>{predicate.label}</a>).slice(0, maxValues);
                         this.setState({
                             dropdownMenuJsx: <div className="dropdown-menu">{menuItemsJsx}</div>,
                         });
                     } else {
-                        this.setState({dropdownMenuJsx: null});
+                        this.hideDropdownMenu();
                     }
                 },
                 (err) => {
                     console.error(err);
                 });
         } else {
-            this.setState({dropdownMenuJsx: null});
+            this.hideDropdownMenu();
         }
     };
 
     handleItemClick = (event) => {
         this.setState({value: event.target.innerText});
+        this.hideDropdownMenu();
+    };
+
+    hideDropdownMenu = () => {
+        this.setState({dropdownMenuJsx: null});
     };
 
     render() {
