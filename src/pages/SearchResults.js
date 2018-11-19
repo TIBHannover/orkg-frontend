55bfa809594d5192a5d0b994e410d53f991dd858
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-11-19 10:07:53 +0100

    Removed the ID's from the list of search results.

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -70,9 +70,9 @@ export default class SearchResults extends Component {
 
         if (resourcesResultsPresent) {
             const resources = this.state.resources.map(
-                resource => <ShortRecord key={resource.id} header={resource.id}
+                resource => <ShortRecord key={resource.id} header={resource.label}
                             href={`${process.env.PUBLIC_URL}/resource/${encodeURIComponent(resource.id)}`}>
-                    {resource.label}</ShortRecord>
+                    </ShortRecord>
             );
             if (resources.length > 0) {
                 body1 =
@@ -88,9 +88,9 @@ export default class SearchResults extends Component {
         }
         if (predicatesResultsPresent) {
             const predicates = this.state.predicates.map(
-                predicate => <ShortRecord key={predicate.id} header={predicate.id}
+                predicate => <ShortRecord key={predicate.id} header={predicate.label}
                         href={`${process.env.PUBLIC_URL}/predicate/${encodeURIComponent(predicate.id)}`}>
-                        {predicate.label}</ShortRecord>
+                        </ShortRecord>
             );
             if (predicates.length > 0) {
                 body2 = <div> <div> <span><u>Predicates</u> related to: <b>{this.props.term}</b></span> </div> {predicates} </div>;
