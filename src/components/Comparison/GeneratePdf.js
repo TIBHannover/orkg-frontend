7fec8fd5cd78dbf39c1eea9ff4071725aaa76b68
Allard Oelen <oelen@l3s.de>
2019-06-17 17:56:13 +0200

    Finish beta version of exporting

--- src/components/Comparison/GeneratePdf.js
+++ src/components/Comparison/GeneratePdf.js
@@ -1,64 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { DropdownItem } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-const pxToMm = (px) => {
-    return Math.floor(px / document.getElementById('myMm').offsetHeight);
-};
+// FIXME: svg icons look ugly while exporting, so hide them before generating the PDF
+// TODO: currently the PDF file has dimensions based on the table, it is better to 
+// have A4 landscape dimensions and fit the table by resizing it
+class GeneratePdf extends Component {
+    render() {
+        return (
+            <DropdownItem
+                onClick={() => {
+                    const input = document.getElementById(this.props.id);
+                    const inputHeightMm = input.offsetHeight;
+                    const inputWidthMm = input.offsetWidth;
 
-const mmToPx = (mm) => {
-    return document.getElementById('myMm').offsetHeight * mm;
-};
+                    html2canvas(input)
+                        .then((canvas) => {
+                            const imgData = canvas.toDataURL('image/png');
 
-const range = (start, end) => {
-    return Array(end - start).join(0).split(0).map(function (val, id) { return id + start });
-};
+                            let pdf = new jsPDF('l', 'mm', [inputHeightMm, inputWidthMm]);
+                            pdf.addImage(imgData, 'PNG', 0, 5);
+                            pdf.save('ORKG Comparison exported.pdf');
+                        });
+                    ;
 
-const GeneratePdf = ({ id }) => (
-    <div className="tc mb4 mt2">
-        {/*
-            Getting pixel height in milimeters:
-            https://stackoverflow.com/questions/7650413/pixel-to-mm-equation/27111621#27111621
-        */}
-        <div id="myMm" style={{ height: "1mm" }} />
+                }}
+            >
+                Export as PDF
+            </DropdownItem>
+        )
+    }
+}
 
-        <DropdownItem
-            onClick={() => {
-                const input = document.getElementById(id);
-                const inputHeightMm = pxToMm(input.offsetHeight);
-                const inputWidthMm = pxToMm(input.offsetWidth);
-                const a4WidthMm = 297;
-                const a4HeightMm = 210;
-                const a4HeightPx = mmToPx(a4HeightMm);
-                const numPages = inputHeightMm <= a4HeightMm ? 1 : Math.floor(inputHeightMm / a4HeightMm) + 1;
-                console.log({
-                    input, inputHeightMm, a4HeightMm, a4HeightPx, numPages, range: range(0, numPages),
-                    comp: inputHeightMm <= a4HeightMm, inputHeightPx: input.offsetHeight
-                });
-
-                html2canvas(input)
-                    .then((canvas) => {
-                        const imgData = canvas.toDataURL('image/png');
-                        
-                        let pdf;
-                        // Document of a4WidthMm wide and inputHeightMm high
-                        if (inputHeightMm > a4HeightMm) {
-                            // elongated a4 (system print dialog will handle page breaks)
-                            pdf = new jsPDF('l', 'mm', [inputHeightMm + 16, a4WidthMm]);
-                        } else {
-                            // standard a4
-                            pdf = new jsPDF('l', 'mm');
-                        }
-                        pdf.addImage(imgData, 'PNG', 0, 0);
-                        pdf.save(`${id}.pdf`);
-                    });
-                ;
-
-            }}
-        >
-            Export as PDF
-        </DropdownItem>
-    </div>);
+GeneratePdf.propTypes = {
+    id: PropTypes.string.isRequired,
+}
 
 export default GeneratePdf;
