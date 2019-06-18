fa09307a70d662b68f8646117dbd76359dd6ae13
K.E Farfar <kfarfar@gmail.com>
2019-06-18 11:58:51 +0200

    add props of PaperCard ad use if in papers page

--- src/components/PaperCard/PaperCard.js
+++ src/components/PaperCard/PaperCard.js
@@ -32,29 +32,36 @@ const PaperCardStyled = styled.div`
 class PaperCard extends Component {
     render() {
         return (
-            <PaperCardStyled className={'list-group-item list-group-item-action ' + (this.props.comparison.allIds.includes(this.props.contribution.subject.id) ? 'selected' : '')}>
+            <PaperCardStyled className={'list-group-item list-group-item-action ' + (this.props.contribution && this.props.comparison.allIds.includes(this.props.contribution.id) ? 'selected' : '')}>
                 <Row>
                     <Col sm="9">
-                        <Link to={reverse(ROUTES.VIEW_PAPER_CONTRIBUTION, { resourceId: this.props.contribution.papers[0].subject.id, contributionId: this.props.contribution.subject.id })}>
-                            {this.props.contribution.papers[0].subject.label}
-                        </Link>
+                        {this.props.contribution && (
+                            <Link to={reverse(ROUTES.VIEW_PAPER_CONTRIBUTION, { resourceId: this.props.paper.id, contributionId: this.props.contribution.id })}>
+                                {this.props.paper.title}
+                            </Link>)}
+                        {!this.props.contribution && (
+                            <Link to={reverse(ROUTES.VIEW_PAPER, { resourceId: this.props.paper.id })}>
+                                {this.props.paper.title}
+                            </Link>)}
                         <br />
                         <small>
-                            <Icon size={'sm'} icon={faUser} /> {this.props.contribution.papers[0].data.authorNames.join(', ')} -
+                            <Icon size={'sm'} icon={faUser} /> {this.props.paper.authorNames.join(', ')} -
                             <i>
-                                {` ${moment(this.props.contribution.papers[0].data.publicationMonth, 'M').format('MMMM')} ${this.props.contribution.papers[0].data.publicationYear} `}
+                                {` ${moment(this.props.paper.publicationMonth, 'M').format('MMMM')} ${this.props.paper.publicationYear} `}
                             </i>
                         </small>
                     </Col>
                     <Col sm="3">
-                        <div className="options" >
-                            <AddToComparison
-                                contributionId={this.props.contribution.subject.id}
-                                paperId={this.props.contribution.papers[0].subject.id}
-                                paperTitle={this.props.contribution.papers[0].subject.label}
-                                contributionTitle={this.props.contribution.subject.label}
-                            />
-                        </div>
+                        {this.props.contribution && (
+                            <div className="options" >
+                                <AddToComparison
+                                    contributionId={this.props.contribution.id}
+                                    paperId={this.props.paper.id}
+                                    paperTitle={this.props.paper.title}
+                                    contributionTitle={this.props.contribution.title}
+                                />
+                            </div>
+                        )}
                     </Col>
                 </Row>
             </PaperCardStyled>
@@ -63,7 +70,17 @@ class PaperCard extends Component {
 }
 
 PaperCard.propTypes = {
-    contribution: PropTypes.object.isRequired,
+    paper: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string,
+        authorNames: PropTypes.array,
+        publicationMonth: PropTypes.string,
+        publicationYear: PropTypes.string,
+    }).isRequired,
+    contribution: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string,
+    }),
     comparison: PropTypes.object.isRequired,
 }
 
