71de516f69a3e854680df47882f6d7fa1005b60c
Allard Oelen <oelen@l3s.de>
2019-07-23 12:25:54 +0200

    Added label to textarea, fix typos

--- src/components/AddPaper/Abstract/Abstract.js
+++ src/components/AddPaper/Abstract/Abstract.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Alert, Card, CardBody } from 'reactstrap';
+import { Button, Alert, Card, CardBody, Label } from 'reactstrap';
 import { arxivUrl } from '../../../network';
 import { connect } from 'react-redux';
 import { updateAbstract, nextStep, previousStep } from '../../../actions/addPaper';
@@ -7,6 +7,7 @@ import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import Textarea from 'react-textarea-autosize';
+import Tooltip from '../../Utils/Tooltip';
 
 class ResearchField extends Component {
     constructor(props) {
@@ -80,14 +81,10 @@ class ResearchField extends Component {
             <div>
                 <h2 className="h4 mt-4 mb-3">Abstract annotation</h2>
 
-                {this.props.abstract && !this.state.changeAbstract ?
+                {this.props.abstract && !this.state.changeAbstract &&
                     <Alert color="info">
                         <strong>Info:</strong> we automatically annotated the abstract for you. Please remove any incorrect annotations
-                    </Alert> 
-                    : 
-                    <Alert color="info">
-                        <strong>Info:</strong> please enter the abstract of your paper
-                    </Alert> 
+                    </Alert>
                 }
 
                 <Card>
@@ -97,12 +94,25 @@ class ResearchField extends Component {
                         {!this.state.changeAbstract ?
                             <p className="pl-2 pr-2">{this.props.abstract}</p>
                             :
-                            <Textarea className="form-control pl-2 pr-2" minRows="5" value={this.props.abstract} onChange={this.handleChange} />
+                            <div>
+                                <Label for="paperAbstract">
+                                    <Tooltip message="Enter the paper abstract to get automatically generated concepts for you paper. You can skip this step by clicking the 'Next step' button">
+                                        Enter the paper abstract
+                                    </Tooltip>
+                                </Label>
+                                <Textarea 
+                                    id="paperAbstract"
+                                    className="form-control pl-2 pr-2" 
+                                    minRows="5" 
+                                    value={this.props.abstract} 
+                                    onChange={this.handleChange} 
+                                />
+                            </div>
                         }
                     </CardBody>
                 </Card>
 
-                <Button color="light" className="mb-2 mt-1" onClick={this.handleChangeAbstract}>{this.state.changeAbstract ? 'Annotate abstact' : 'Change abstact'}</Button>
+                <Button color="light" className="mb-2 mt-1" onClick={this.handleChangeAbstract}>{this.state.changeAbstract ? 'Annotate abstract' : 'Change abstract'}</Button>
 
                 <hr className="mt-5 mb-3" />
 
