e71fdc1da2c4829139413b1a1712695190e5c432
K.E Farfar <kfarfar@gmail.com>
2019-08-01 12:35:25 +0200

    Fix regex of link value plugin

--- src/components/ValuePlugins/Link/Link.js
+++ src/components/ValuePlugins/Link/Link.js
@@ -7,8 +7,7 @@ import { renderToString } from 'react-dom/server';
 class Link extends Component {
     constructor(props) {
         super(props);
-
-        const expression = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;        
+        const expression = /(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/gi;        
         this.supportedValues = new RegExp(expression);
     }
 
