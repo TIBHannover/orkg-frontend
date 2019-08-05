254c1dc625f99704e0146367b5ae2adada95bebc
K.E Farfar <kfarfar@gmail.com>
2019-08-05 15:29:41 +0200

    Add activity feed

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
 import { Container, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
@@ -7,6 +8,35 @@ import Gravatar from 'react-gravatar';
 
 const StyledGravatar = styled(Gravatar)`
   border: 3px solid ${(props) => props.theme.avatarBorderColor};
+`;
+
+const StyledActivity = styled.div`
+  border-left: 3px solid #e9ebf2;
+  color: ${(props) => props.theme.bodyColor};
+  .time {
+    color: rgba(100, 100, 100, 0.57);
+    margin-top: -0.2rem;
+    margin-bottom: 0.2rem;
+    font-size: 15px;
+  }
+  .time::before {
+    width: 1rem;
+    height: 1rem;
+    margin-left: -1.6rem;
+    margin-right: 0.5rem;
+    border-radius: 15px;
+    content: '';
+    background-color: #c2c6d6;
+    display: inline-block;
+  }
+  a {
+    color: ${(props) => props.theme.ORKGPrimaryColor};
+  }
+
+  &:last-child {
+    border-left: none;
+    padding-left: 1.2rem !important;
+  }
 `;
 
 class UserProfile extends Component {
@@ -44,6 +74,25 @@ class UserProfile extends Component {
       </Container>
       <Container className="box mt-4 pt-4 pb-3 pl-5 pr-5">
         <h5 className="mb-4">Activity feed</h5>
+        <StyledActivity className="pl-3 pb-3">
+          <div className={'time'}>16 JULY 2019</div>
+          <div>
+            John Doe updated resource <Link to={'/'}>IoT research directions</Link>
+          </div>
+        </StyledActivity>
+        <StyledActivity className="pl-3 pb-3">
+          <div className={'time'}>10 JULY 2019</div>
+          <div>
+            John Doe updated resource <Link to={'/'}>IoT research directions</Link>
+          </div>
+          <div>
+            John Doe commented on predicate <Link to={'/'}>Has Problem</Link>
+          </div>
+        </StyledActivity>
+        <StyledActivity className="pl-3 pb-3">
+          <div className={'time'}>5 JULY 2019</div>
+          <div>John Doe joined ORKG, welcome!</div>
+        </StyledActivity>
       </Container>
     </>
   );
