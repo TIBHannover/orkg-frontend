a94b490f70d74ed78808536f31ef2ea2ec02d6f6
K.E Farfar <kfarfar@gmail.com>
2019-10-18 17:37:09 +0200

    Integrate pagination in tools/resources page

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ShortRecord from '../components/statements/ShortRecord';
 import { Link } from 'react-router-dom';
-import { submitGetRequest, url } from '../network';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { getAllResources } from '../network';
 import { Container } from 'reactstrap';
 
 export default class Resources extends Component {
@@ -9,62 +11,94 @@ export default class Resources extends Component {
     constructor(props) {
         super(props);
 
+        this.pageSize = 25;
+
         this.state = {
-            allResources: null,
+            resources: [],
             results: null,
-            error: null,
+            isNextPageLoading: false,
+            hasNextPage: false,
+            page: 1,
+            isLastPageReached: false,
         };
-
     }
 
-    async componentDidMount() {
-        await this.findAllResources();
+    componentDidMount() {
+        this.loadMoreResources();
     }
 
-    findAllResources = async () => {
-        try {
-            const responseJson = await submitGetRequest(url + 'resources/');
-
-            this.setState({
-                allResources: responseJson,
-                error: null,
-            });
-        } catch (err) {
-            console.error(err);
-            this.setState({
-                allResources: null,
-                error: err.message,
-            });
-        }
-    };
+    loadMoreResources = () => {
+        this.setState({ isNextPageLoading: true })
+        getAllResources({
+            page: this.state.page,
+            items: this.pageSize,
+            sortBy: 'id',
+            desc: true
+        }).then((resources) => {
+            if (resources.length > 0) {
+                this.setState({
+                    resources: [...this.state.resources, ...resources],
+                    isNextPageLoading: false,
+                    hasNextPage: resources.length < this.pageSize ? false : true,
+                    page: this.state.page + 1
+                });
+            } else {
+                this.setState({
+                    isNextPageLoading: false,
+                    hasNextPage: false,
+                    isLastPageReached: true
+                });
+            }
+        });
+    }
 
     render() {
-        const resultsPresent = this.state.error || (this.state.allResources);
-
-        if (this.state.error) {
-            return <p><strong>Error:</strong> {this.state.error} </p>;
-        }
-
-        if (resultsPresent) {
-            const resources = this.state.allResources.map(
-                resource => (<ShortRecord key={resource.id} header={resource.label}
-                    href={`${process.env.PUBLIC_URL}/resource/${encodeURIComponent(resource.id)}`}
-                             />)
-            );
-
-            return (<Container className="box pt-4 pb-4 pl-5 pr-5 mt-5">
-                <div className="addResource toolbar addToolbar-container">
-                    <span className="toolbar-button toolbar-button-add">
-                        <Link to={`${process.env.PUBLIC_URL}/addResource`}>
-                            <span className="fa fa-plus" />add new resource
-                        </Link>
-                    </span>
-                </div>
-                {resources}
-                    </Container>)
-        } else {
-            return null;
-        }
+        return (
+            <>
+                <Container className="p-0">
+                    <h1 className="h4 mt-4 mb-4">View all resources</h1>
+                </Container>
+                <Container className={'box pt-4 pb-4 pl-5 pr-5 clearfix'}>
+                    <div className="addResource toolbar addToolbar-container">
+                        <span className="toolbar-button toolbar-button-add">
+                            <Link to={`${process.env.PUBLIC_URL}/addResource`}>
+                                <span className="fa fa-plus" />add new resource
+                            </Link>
+                        </span>
+                    </div>
+                    {this.state.resources.length > 0 &&
+                        <div>
+                            {this.state.resources.map(
+                                (predicate) => {
+                                    return (
+                                        <ShortRecord
+                                            key={predicate.id}
+                                            header={predicate.id}
+                                            href={`${process.env.PUBLIC_URL}/predicate/${encodeURIComponent(predicate.id)}`}
+                                        >
+                                            {predicate.label}
+                                        </ShortRecord>
+                                    )
+                                }
+                            )}
+                        </div>
+                    }
+                    {this.state.resources.length === 0 && !this.state.isNextPageLoading &&
+                        <div className="text-center mt-4 mb-4">No Resources</div>
+                    }
+                    {this.state.isNextPageLoading && <div className="text-center mt-4 mb-4"><Icon icon={faSpinner} spin /> Loading</div>}
+                    {!this.state.isNextPageLoading && this.state.hasNextPage && (
+                        <div style={{ cursor: 'pointer' }} className="list-group-item list-group-item-action text-center mt-2" onClick={!this.state.isNextPageLoading ? this.loadMoreResources : undefined}>
+                            Load more resources
+                        </div>
+                    )}
+                    {!this.state.hasNextPage && this.state.isLastPageReached && (
+                        <div className="text-center mt-3">
+                            You have reached the last page.
+                        </div>)
+                    }
+                </Container>
+            </>
+        )
     }
-
 }
