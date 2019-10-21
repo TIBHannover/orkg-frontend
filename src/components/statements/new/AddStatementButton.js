bf4a4d80e390450ca59eb800a3a0228660bc08a9
K.E Farfar <kfarfar@gmail.com>
2019-10-21 11:35:25 +0200

    Fix NewStatementsSection class

--- src/components/statements/new/AddStatementButton.js
+++ src/components/statements/new/AddStatementButton.js
@@ -1,17 +1,25 @@
-/* eslint-disable */
-import React, {Component} from 'react';
-import {Button} from 'reactstrap';
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-export default class AddStatementButton extends Component {
+class AddStatementButton extends Component {
 
     render() {
-        return (<div className="addToolbar toolbar addToolbar-container toolbar-container">
-            <span className="toolbar-button toolbar-button-add">
-                <Button onClick={this.props.onClick}>
-                    <span className="fa fa-plus" />add statement
-                </Button>
-            </span>
-                </div>)
+        return (
+            <div className="addToolbar toolbar addToolbar-container toolbar-container">
+                <span className="toolbar-button toolbar-button-add">
+                    <Button onClick={this.props.onClick}>
+                        <span className="fa fa-plus" />add statement
+                    </Button>
+                </span>
+            </div>
+        )
     }
 
 }
+
+AddStatementButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
+};
+
+export default AddStatementButton;
