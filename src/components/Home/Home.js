d8612bf76327befc624a8774a412ecb4cfafba6e
K.E Farfar <kfarfar@gmail.com>
2019-11-06 15:27:51 +0100

    style(home): add a banner that links to project page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle, faStar, faPlus, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faPlus } from '@fortawesome/free-solid-svg-icons';
 import ResearchFieldCards from './ResearchFieldCards';
 import RecentlyAddedPapers from './RecentlyAddedPapers';
+import HomeBannerBg from 'assets/img/home_banner_bg.jpg';
+import styled from 'styled-components';
 
+const StyledAbout = styled.div`
+    h1{
+        color: ${(props) => props.theme.darkblue};
+    }
+    h1 span{
+        color: ${(props) => props.theme.orkgPrimaryColor};
+    }
+    .aboutContent{
+        border-radius: 5px;
+        background: #fff url(${HomeBannerBg}) no-repeat right center;
+    }
+    .aboutContent > .row{
+        display:table;
+        width: calc(100% + 30px);
+    }
+    .aboutContent > .row > [class^="col-"],
+    .aboutContent > .row > [class*=" col-"]{
+        float:none;
+        display:table-cell;
+        vertical-align:middle;
+    }
+
+    @media (max-width: 991px){
+        .aboutContent{
+            background: #fff;
+        }
+        .aboutContent > .row{
+            display:block;
+            width: auto;
+        }
+        .aboutContent > .row > [class^="col-"],
+        .aboutContent > .row > [class*=" col-"]{
+            float:none;
+            display:block;
+            vertical-align:middle;
+            position: relative;
+        }
+    }
+`
 class Home extends Component {
 
     componentDidMount = () => {
         document.title = 'Open Research Knowledge Graph'
     }
 
-    render = () => {       
+    render = () => {
         return (
             <div>
-                <Container className="p-0">
-                    <h1 className="h4 mt-4 mb-4">Homepage</h1>
-                </Container>
-                <Container className="box pt-4 pb-4 pl-5 pr-5">
-                    <Icon icon={faInfoCircle} className="text-primary" /> The <strong>Open Research Knowledge Graph</strong> - or - ORKG aims to describe research papers and contributions in a structured manner. With ORKG research contributions become findable and comparable. In order to add your own research, or to contribute,
-                {' '}<a href="https://projects.tib.eu/orkg/" target="_blank" rel="noopener noreferrer">learn more <Icon icon={faExternalLinkAlt} /></a>
-                </Container>
+                <StyledAbout>
+                    <Container className="box pt-4 pb-4 pl-4 pr-4 aboutContent">
+                        <div className="row">
+                            <div className="col-md-9">
+                                <h1><span>O</span>pen <span>R</span>esearch <span>K</span>nowledge <span>G</span>raph</h1>
+                                <div className="mt-4">
+                                    <p className="mb-0">The ORKG aims to describe research papers and contributions in a structured manner. With ORKG research contributions become findable and comparable. In order to add your own research, or to contribute,</p>
+                                </div>
+                            </div>
+                            <div className="col-md-3 about-link">
+                                <a href="https://projects.tib.eu/orkg/" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-lg btn-block btn-default">Learn More!</a>
+                            </div>
+                        </div>
+                    </Container>
+                </StyledAbout>
 
                 <Container className="mt-4">
                     <Row>
