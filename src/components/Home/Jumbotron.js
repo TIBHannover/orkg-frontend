1fd7cfc36c7b5716c59fa75169c2d2eacd9e73ab
Allard Oelen <oelen@l3s.de>
2019-12-13 16:00:45 +0100

    Small style changes

--- src/components/Home/Jumbotron.js
+++ src/components/Home/Jumbotron.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import HomeBannerBg from 'assets/img/graph-background.svg';
 import styled from 'styled-components';
-import Typist from 'react-typist';
 
 const JumbotronStyled = styled.div`
     padding: 100px 0;
     color: hsla(0, 0%, 100%, 0.6);
-    background: ${props => props.theme.darkblueDarker} url(${HomeBannerBg});
+    background: rgb(95, 100, 116) url(${HomeBannerBg});
     background-position-x: 0%, 0%;
     background-position-y: 0%, 0%;
     background-size: auto, auto;
@@ -24,15 +23,22 @@ const JumbotronStyled = styled.div`
     }
 `;
 
+const HeaderStyled = styled.h1`
+    text-shadow: 0px 3px #373c4aa6;
+`;
+
 export default class Jumbotron extends Component {
     render() {
         return (
             <div>
                 <JumbotronStyled className="pt-6 pb-6">
                     <Container className="position-relative text-center">
-                        <h1 className="mb-3 text-white">Scholarly Knowledge. Structured</h1>
+                        <HeaderStyled className="mb-3 text-white">Scholarly Knowledge. Structured.</HeaderStyled>
                         <div className="col-md-8 mx-auto mb-5 marketingtext">
-                            <p className="mr-n2 ml-n2">The Open Research Knowledge Graph aims to describe research papers in a structured manner.</p>
+                            <p className="mr-n2 ml-n2">
+                                The Open Research Knowledge Graph (ORKG) aims to describe research papers in a structured manner. With the ORKG,
+                                papers because findable and comparible.{' '}
+                            </p>
                         </div>
 
                         <div style={{ position: 'relative' }}>
@@ -42,7 +48,7 @@ export default class Jumbotron extends Component {
                                         href="https://projects.tib.eu/orkg/"
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="btn btn-darkblue btn-lg btn-block btn-default btn-sm"
+                                        className="btn btn-darkblue btn-block btn-default"
                                         style={{ width: 150 }}
                                     >
                                         Learn More
