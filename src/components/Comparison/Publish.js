27f3e7b7966de5fb28bdb197919a3fd5e49015de
K.E Farfar <kfarfar@gmail.com>
2020-01-17 17:32:41 +0100

    fix(Comparison): Comparison url

--- src/components/Comparison/Publish.js
+++ src/components/Comparison/Publish.js
@@ -56,9 +56,10 @@ class Publish extends Component {
     };
 
     render() {
-        const comparisonLink = `${window.location.protocol}//${window.location.host}${reverse(ROUTES.COMPARISON, {
-            comparisonId: this.props.comparisonId || this.state.comparisonId
-        })}`;
+        const comparisonLink = `${window.location.protocol}//${window.location.host}${window.location.pathname.replace(
+            reverse(ROUTES.COMPARISON, { comparisonId: this.props.comparisonId }),
+            ''
+        )}${reverse(ROUTES.COMPARISON, { comparisonId: this.props.comparisonId || this.state.comparisonId })}`;
 
         return (
             <Modal isOpen={this.props.showDialog} toggle={this.props.toggle}>
