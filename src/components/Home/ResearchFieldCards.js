76d9d9950555e3ecdc1d5adc44f24162b362f05f
Allard Oelen <oelen@l3s.de>
2020-01-21 13:02:28 +0100

    feat(ResearchFields): grey out empty fields, better UX with animations

--- src/components/Home/ResearchFieldCards.js
+++ src/components/Home/ResearchFieldCards.js
@@ -6,6 +6,8 @@ import { getStatementsBySubject, getStatementsByObject } from '../../network';
 import { Link } from 'react-router-dom';
 import { reverse } from 'named-urls';
 import ROUTES from '../../constants/routes.js';
+import classNames from 'classnames';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 /* Bootstrap card column is not working correctly working with vertical alignment,
 thus used custom styling here */
@@ -16,13 +18,29 @@ const Card = styled.div`
     color: #fff !important;
     border: 0 !important;
     border-radius: 12px !important;
-    min-height: 75px;
+    min-height: 85px;
     flex: 0 0 calc(33% - 20px) !important;
     margin: 10px;
     transition: opacity 0.2s;
+    justify-content: center;
+    display: flex;
+    flex: 1 1 auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
 
     &:hover {
         opacity: 0.8;
+    }
+    &[disabled] {
+        opacity: 0.5;
+        cursor: default;
+        pointer-events: none;
+    }
+    &:active {
+        top: 4px;
     }
 `;
 
@@ -38,6 +56,26 @@ const BreadcrumbLink = styled.span`
 
     &:hover {
         text-decoration: underline;
+    }
+`;
+
+const PaperAmount = styled.div`
+    opacity: 0.5;
+    font-size: 80%;
+    text-align: center;
+`;
+
+const AnimationContainer = styled(CSSTransition)`
+    //transition: 0.3s background-color, 0.3s border-color;
+
+    animation: scale-up-center 0.4s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+    @keyframes scale-up-center {
+        0% {
+            transform: scale(0.5);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
 `;
 
@@ -147,17 +185,20 @@ class ResearchFieldCards extends Component {
                 ))}
 
                 <hr className="mt-3 mb-5" />
-                <div id="research-field-cards" className="mt-2 justify-content-center d-flex flex-wrap">
-                    {this.state.researchFields.map(field => (
-                        <Card
-                            className="card card-body p-0 justify-content-center"
-                            role="button"
-                            key={field.id}
-                            onClick={() => this.getFields(field.id, field.label)}
-                        >
-                            <CardTitle className="card-title m-0 text-center">{field.label}</CardTitle>
-                        </Card>
-                    ))}
+                <div>
+                    <TransitionGroup id="research-field-cards" className="mt-2 justify-content-center d-flex flex-wrap" exit={false}>
+                        {this.state.researchFields.map(field => {
+                            const disabled = Math.round(Math.random());
+                            return (
+                                <AnimationContainer key={field.id} classNames="fadeIn" timeout={{ enter: 500, exit: 0 }}>
+                                    <Card role="button" disabled={disabled} onClick={() => this.getFields(field.id, field.label)}>
+                                        <CardTitle className="card-title m-0 text-center">{field.label}</CardTitle>
+                                        <PaperAmount>234 papers</PaperAmount>
+                                    </Card>
+                                </AnimationContainer>
+                            );
+                        })}
+                    </TransitionGroup>
                 </div>
                 {showPapers && (
                     <div>
