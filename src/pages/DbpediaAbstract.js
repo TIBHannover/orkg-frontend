35ad8874ec3a0a48948d14126a31804771ed1d07
Allard Oelen <oelen@l3s.de>
2020-01-23 12:20:25 +0100

    feat(Resource): show DBpedia abstract when resource is sameAs DBpedia resource, add 'Open resource' link to comparison

---
+++ src/pages/DbpediaAbstract.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+class DbpediaAbstract extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            abstract: '',
+            collapsed: true
+        };
+
+        this.getAbstract();
+    }
+
+    getAbstract = async () => {
+        const resource = this.props.externalResource;
+        const endpoint = 'http://dbpedia.org/sparql';
+        const query = `
+            SELECT
+                ?abstract
+            WHERE 
+                {
+                    <${resource}> <http://dbpedia.org/ontology/abstract> ?abstract .
+                    FILTER (lang(?abstract) = 'en')
+                }
+            LIMIT 500
+        `;
+
+        const url = endpoint + '?query=' + encodeURIComponent(query) + '&format=json';
+        const self = this;
+
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                if (
+                    data.results &&
+                    data.results.bindings &&
+                    data.results.bindings.length > 0 &&
+                    data.results.bindings[0].abstract &&
+                    data.results.bindings[0].abstract.value
+                ) {
+                    self.setState({
+                        abstract: data.results.bindings[0].abstract.value
+                    });
+                }
+            });
+    };
+
+    handleReadMore = () => {
+        this.setState(prevState => ({
+            collapsed: !prevState.collapsed
+        }));
+    };
+
+    render() {
+        const shortAbstract = this.state.abstract.substr(0, 550);
+        const showReadMore = this.state.abstract !== shortAbstract;
+
+        return (
+            <div className="mt-5 mb-2 list-group-item">
+                <h2 className="h5 mt-2 float-left">Abstract from DBpedia</h2>
+                <a href={this.props.externalResource} target="_blank" rel="noopener noreferrer">
+                    <img
+                        alt="DBpedia logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/DBpediaLogo.svg/1200px-DBpediaLogo.svg.png"
+                        style={{ height: 40, float: 'right' }}
+                    />
+                </a>
+                <div className="clearfix" />
+
+                <div style={{ fontSize: '90%' }}>
+                    {this.state.collapsed ? shortAbstract + '...' : this.state.abstract}
+                    {showReadMore && (
+                        <Button color="link" className="p-0" style={{ fontSize: 'inherit' }} onClick={this.handleReadMore}>
+                            {this.state.collapsed ? 'Read more' : 'Read less'}
+                        </Button>
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+DbpediaAbstract.propTypes = {
+    externalResource: PropTypes.string.isRequired
+};
+
+export default DbpediaAbstract;
---
+++ src/pages/SameAsStatements.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import DbpediaAbstract from './DbpediaAbstract';
+
+class SameAsStatements extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            externalResources: []
+        };
+
+        this.expressionDbPedia = new RegExp(/^(https?:)?\/\/dbpedia\.org(\/resource(\?.*)?)\//);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.values !== this.props.values || prevProps.properties !== this.props.properties) {
+            this.getSameAsResources();
+        }
+    }
+
+    getSameAsResources = () => {
+        const internalSameAsIds =
+            this.props.properties.allIds.length > 0
+                ? this.props.properties.allIds.filter(
+                      property => this.props.properties.byId[property].existingPredicateId === process.env.REACT_APP_PREDICATES_SAME_AS
+                  )
+                : [];
+
+        let valueIds = internalSameAsIds.length > 0 ? internalSameAsIds.map(propertyId => this.props.properties.byId[propertyId].valueIds) : [];
+        valueIds = valueIds.flat();
+
+        const values = this.props.values ? valueIds.map(valueId => this.props.values.byId[valueId].label) : [];
+
+        this.setState({
+            externalResources: values
+        });
+    };
+
+    render() {
+        if (this.state.externalResources.length > 0) {
+            return (
+                <div>
+                    {this.state.externalResources.map((resourceUrl, index) => {
+                        if (resourceUrl.match(this.expressionDbPedia)) {
+                            return <DbpediaAbstract externalResource={resourceUrl} key={index} />;
+                        }
+                        return <></>;
+                    })}
+                </div>
+            );
+        }
+        return null;
+    }
+}
+
+SameAsStatements.propTypes = {
+    properties: PropTypes.object.isRequired,
+    values: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+    return {
+        level: state.statementBrowser.level,
+        resources: state.statementBrowser.resources,
+        properties: state.statementBrowser.properties,
+        values: state.statementBrowser.values,
+        isFetchingStatements: state.statementBrowser.isFetchingStatements,
+        selectedResource: state.statementBrowser.selectedResource
+    };
+};
+
+export default connect(mapStateToProps)(SameAsStatements);
