4c99820b9f50a926c25100e9f1b1086c17d52073
K.E Farfar <kfarfar@gmail.com>
2020-03-17 17:59:49 +0100

    Edit the researh field of a paper

--- src/components/ViewPaper/EditDialog/EditItem.js
+++ src/components/ViewPaper/EditDialog/EditItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Collapse, Input } from 'reactstrap';
+import AuthorsInput from 'components/Utils/AuthorsInput';
+import AutoComplete from 'components/Utils/AutoComplete';
 import { StyledStatementItem, StyledListGroupOpen } from 'components/AddPaper/Contributions/styled';
 import classNames from 'classnames';
 import moment from 'moment';
 import { range } from 'utils';
+import { truncate } from 'lodash';
 import { resourcesUrl } from 'network';
-import AuthorsInput from '../../Utils/AuthorsInput';
-import AutoComplete from '../../Utils/AutoComplete';
 import PropTypes from 'prop-types';
 
 const EditItem = props => {
@@ -23,9 +24,11 @@ const EditItem = props => {
     });
 
     let input;
+    let stringValue;
 
     if (props.type === 'text') {
         input = <Input value={props.value} onChange={props.onChange} />;
+        stringValue = truncate(props.value, { length: 60 });
     } else if (props.type === 'month') {
         input = (
             <Input type="select" value={props.value} onChange={props.onChange}>
@@ -38,6 +41,7 @@ const EditItem = props => {
                 })}
             </Input>
         );
+        stringValue = moment(props.value + 1, 'M').format('MMMM');
     } else if (props.type === 'year') {
         input = (
             <Input type="select" value={props.value} onChange={props.onChange}>
@@ -48,8 +52,11 @@ const EditItem = props => {
                     ))}
             </Input>
         );
+        stringValue = props.value;
     } else if (props.type === 'authors') {
         input = <AuthorsInput value={props.value} handler={props.onChange} />;
+        const authors = props.value.map(author => author.label);
+        stringValue = authors.length > 2 ? `${authors.slice(0, 2).join(', ')} et al.` : authors.join(', ');
     } else if (props.type === 'publishedIn') {
         input = (
             <AutoComplete
@@ -64,12 +71,34 @@ const EditItem = props => {
                 isClearable={true}
             />
         );
+        stringValue = props.value ? truncate(props.value.label, { length: 60 }) : 'Not specified';
+    } else if (props.type === 'researchField') {
+        input = (
+            <AutoComplete
+                allowCreate={false}
+                requestUrl={resourcesUrl}
+                optionsClass={process.env.REACT_APP_CLASSES_RESEARCH_FIELD}
+                onItemSelected={props.onChange}
+                placeholder={'Select a research field'}
+                autoFocus
+                cacheOptions
+                value={props.value ? props.value : null}
+                isClearable={false}
+            />
+        );
+        stringValue = props.value && props.value.label ? props.value.label : 'Not specified';
     }
 
     return (
         <>
             <StyledStatementItem className={listGroupClass} onClick={props.toggleItem}>
-                {props.label}
+                {props.open ? (
+                    props.label
+                ) : (
+                    <>
+                        {props.label} : {stringValue ? <i>{stringValue}</i> : 'Not specified'}
+                    </>
+                )}
             </StyledStatementItem>
             <Collapse isOpen={props.open}>
                 <StyledListGroupOpen className={openBoxClass}>{input}</StyledListGroupOpen>
@@ -84,7 +113,7 @@ EditItem.propTypes = {
     toggleItem: PropTypes.func.isRequired,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array, PropTypes.object]),
     onChange: PropTypes.func.isRequired,
-    type: PropTypes.oneOf(['text', 'month', 'year', 'authors', 'publishedIn']).isRequired,
+    type: PropTypes.oneOf(['text', 'month', 'year', 'authors', 'publishedIn', 'researchField']).isRequired,
     isLastItem: PropTypes.bool
 };
 
---
+++ src/components/ViewPaper/PaperHeader.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Badge } from 'reactstrap';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faUser, faCalendar, faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { reverse } from 'named-urls';
+import ROUTES from 'constants/routes';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import EditPaperDialog from './EditDialog/EditPaperDialog';
+
+class PaperHeader extends Component {
+    render() {
+        return (
+            <>
+                <div className="d-flex align-items-start">
+                    <h2 className="h4 mt-4 mb-3 flex-grow-1">{this.props.viewPaper.title ? this.props.viewPaper.title : <em>No title</em>}</h2>
+                </div>
+
+                <div className="clearfix" />
+
+                {this.props.viewPaper.publicationMonth || this.props.viewPaper.publicationYear ? (
+                    <span className="badge badge-lightblue mr-2">
+                        <Icon icon={faCalendar} className="text-primary" />{' '}
+                        {this.props.viewPaper.publicationMonth ? moment(this.props.viewPaper.publicationMonth, 'M').format('MMMM') : ''}{' '}
+                        {this.props.viewPaper.publicationYear ? this.props.viewPaper.publicationYear : ''}
+                    </span>
+                ) : (
+                    ''
+                )}
+                {this.props.viewPaper.researchField && (
+                    <Link to={reverse(ROUTES.RESEARCH_FIELD, { researchFieldId: this.props.viewPaper.researchField.id })}>
+                        <span className="badge badge-lightblue mr-2 mb-2">
+                            <Icon icon={faBars} className="text-primary" /> {this.props.viewPaper.researchField.label}
+                        </span>
+                    </Link>
+                )}
+                {this.props.viewPaper.authors.map((author, index) =>
+                    author.classes && author.classes.includes(process.env.REACT_APP_CLASSES_AUTHOR) ? (
+                        <Link key={index} to={reverse(ROUTES.AUTHOR_PAGE, { authorId: author.id })}>
+                            <Badge color="lightblue" className="mr-2 mb-2" key={index}>
+                                <Icon icon={faUser} className="text-primary" /> {author.label}
+                            </Badge>
+                        </Link>
+                    ) : (
+                        <Badge color="lightblue" className="mr-2 mb-2" key={index}>
+                            <Icon icon={faUser} className="text-darkblue" /> {author.label}
+                        </Badge>
+                    )
+                )}
+                <br />
+                <div className="d-flex justify-content-end align-items-center">
+                    {this.props.viewPaper.publishedIn && (
+                        <div className="flex-grow-1">
+                            <small>
+                                Published in:{' '}
+                                <Link
+                                    style={{ color: '#60687a', fontStyle: 'italic' }}
+                                    to={reverse(ROUTES.VENUE_PAGE, { venueId: this.props.viewPaper.publishedIn.id })}
+                                >
+                                    {this.props.viewPaper.publishedIn.label}
+                                </Link>
+                            </small>
+                        </div>
+                    )}
+                    {this.props.viewPaper.doi && this.props.viewPaper.doi.startsWith('10.') && (
+                        <div className="flex-shrink-0">
+                            <small>
+                                DOI:{' '}
+                                <a href={`https://doi.org/${this.props.viewPaper.doi}`} target="_blank" rel="noopener noreferrer">
+                                    {this.props.viewPaper.doi}
+                                </a>
+                            </small>
+                        </div>
+                    )}
+                </div>
+                {this.props.editMode && <EditPaperDialog />}
+            </>
+        );
+    }
+}
+
+PaperHeader.propTypes = {
+    location: PropTypes.object.isRequired,
+    viewPaper: PropTypes.object.isRequired,
+    editMode: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+    viewPaper: state.viewPaper
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PaperHeader);
