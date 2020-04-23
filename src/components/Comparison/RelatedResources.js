597a88a144eaa254e6048388da9ca7c19b83426e
Allard Oelen <oelen@l3s.de>
2020-04-23 11:46:34 +0200

    feat: thumbnail, title and description for related comparison resources

--- src/components/Comparison/RelatedResources.js
+++ src/components/Comparison/RelatedResources.js
@@ -1,38 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getStatementsBySubject } from 'network';
+import { Card, CardImg, CardText, CardBody, CardTitle, Button, CardColumns } from 'reactstrap';
 
-const RelatedResources = props => {
-    // eslint-disable-next-line no-useless-escape
-    const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/gi;
+class RelatedResources extends Component {
+    constructor(props) {
+        super(props);
+        // eslint-disable-next-line no-useless-escape
+        this.urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/gi;
 
-    return (
-        props.resources.length > 0 && (
-            <>
-                <h3 className="mt-5 h5">Related resources</h3>
-                <ul>
-                    {props.resources.map(resource => {
-                        const isLink = new RegExp(urlRegex).test(resource);
+        this.state = {
+            relatedResources: []
+        };
+    }
 
-                        return (
-                            <li key={resource}>
-                                {isLink ? (
-                                    <a href={resource} target="_blank" rel="noopener noreferrer">
-                                        {resource}
-                                    </a>
-                                ) : (
-                                    resource
-                                )}
-                            </li>
+    componentDidMount() {
+        this.loadResources();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.resourcesStatements !== prevProps.resourcesStatements) {
+            this.loadResources();
+        }
+    }
+
+    // Support for related resources in two different formats:
+    // 1) Comparison -[RelatedResource]-> "Literal url"
+    // 1) Comparison -[RelatedResource]-> Resource
+    //    Resource -[Url]-> "Literal url"
+    //    Resource -[Image]-> "Literal, base64 encoded image"
+    loadResources = async () => {
+        if (this.props.resourcesStatements.length > 0) {
+            const relatedResources = [];
+
+            for (const resource of this.props.resourcesStatements) {
+                if (resource.object._class === 'literal') {
+                    relatedResources.push({
+                        url: resource.object.label
+                    });
+                } else {
+                    await getStatementsBySubject({ id: resource.object.id }).then(statements => {
+                        const imageStatement = statements.find(statement => statement.predicate.id === process.env.REACT_APP_PREDICATES_IMAGE);
+                        const urlStatement = statements.find(statement => statement.predicate.id === process.env.REACT_APP_PREDICATES_URL);
+                        const titleStatement = statements.find(statement => statement.predicate.id === process.env.REACT_APP_PREDICATES_TITLE);
+                        const descriptionStatement = statements.find(
+                            statement => statement.predicate.id === process.env.REACT_APP_PREDICATES_DESCRIPTION
                         );
-                    })}
-                </ul>
-            </>
-        )
-    );
-};
+
+                        relatedResources.push({
+                            url: urlStatement ? urlStatement.object.label : '',
+                            image: imageStatement ? imageStatement.object.label : '',
+                            title: titleStatement ? titleStatement.object.label : '',
+                            description: descriptionStatement ? descriptionStatement.object.label : ''
+                        });
+                    });
+                }
+            }
+
+            this.setState(prevState => ({
+                relatedResources: [...prevState.relatedResources, ...relatedResources]
+            }));
+        }
+    };
+
+    render() {
+        return (
+            this.state.relatedResources.length > 0 && (
+                <>
+                    <h3 className="mt-5 h5">Related resources</h3>
+                    <CardColumns>
+                        {this.state.relatedResources.map((resource, index) => {
+                            const isLink = new RegExp(this.urlRegex).test(resource.url);
+
+                            return (
+                                <Card>
+                                    {resource.image && <CardImg top width="100%" src={resource.image ? resource.image : ''} alt="Card image cap" />}
+                                    <CardBody>
+                                        {resource.title && <CardTitle>{resource.title}</CardTitle>}
+                                        {resource.description && <CardText>{resource.description}</CardText>}
+                                        {isLink ? (
+                                            <a href={resource.url} target="_blank" rel="noopener noreferrer">
+                                                <Button size="sm" color="darkblue">
+                                                    Visit resource
+                                                </Button>
+                                            </a>
+                                        ) : (
+                                            resource.url
+                                        )}
+                                    </CardBody>
+                                </Card>
+                            );
+                        })}
+                    </CardColumns>
+                </>
+            )
+        );
+    }
+}
 
 RelatedResources.propTypes = {
-    resources: PropTypes.array.isRequired
+    resourcesStatements: PropTypes.array.isRequired
 };
 
 export default RelatedResources;
