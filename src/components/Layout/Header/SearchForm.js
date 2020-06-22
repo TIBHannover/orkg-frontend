0cdcb8034e11d3c92c4824a7683708bf479541fc
Allard Oelen <oelen@l3s.de>
2020-06-22 12:52:27 +0200

    fix(Search): character encoding, refactoring search form

--- src/components/Layout/Header/SearchForm.js
+++ src/components/Layout/Header/SearchForm.js
@@ -1,76 +1,69 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
-import ROUTES from '../../../constants/routes.js';
+import ROUTES from 'constants/routes.js';
 import { reverse } from 'named-urls';
+import { useRouteMatch, useHistory } from 'react-router-dom';
+import { Form, Input, Button, InputGroup, InputGroupAddon } from 'reactstrap';
+import { isString } from 'lodash';
 
-class SearchForm extends Component {
-    constructor(props) {
-        super(props);
+const SearchForm = props => {
+    const PROPERTY_PATTERN = /^#P+[0-9]+/;
+    const RESOURCE_PATTERN = /^#R+[0-9]+/;
+    const MINIMUM_LENGTH_PATTERN = 3;
 
-        this.state = {
-            redirect: false,
-            value: ''
-        };
-    }
+    const [value, setValue] = useState('');
+    const match = useRouteMatch(ROUTES.SEARCH);
+    const urlSearchQuery = match?.params?.searchTerm;
+    const history = useHistory();
 
-    handleChange = event => {
-        this.setState({ value: event.target.value });
+    useEffect(() => {
+        const decodedValue = isString(urlSearchQuery) ? decodeURIComponent(urlSearchQuery) : urlSearchQuery;
+        setValue(decodedValue || '');
+    }, [urlSearchQuery]);
+
+    const handleChange = event => {
+        setValue(event.target.value);
     };
 
-    handleSubmit = e => {
-        this.setState({
-            redirect: true
-        });
-
+    const handleSubmit = e => {
         e.preventDefault();
+
+        let route = '';
+        if (isString(value) && value.length >= MINIMUM_LENGTH_PATTERN && (value.match(RESOURCE_PATTERN) || value.match(PROPERTY_PATTERN))) {
+            const id = value.substring(1);
+            setValue('');
+            route = reverse(value.match(RESOURCE_PATTERN) ? ROUTES.RESOURCE : ROUTES.PREDICATE, { id });
+        } else {
+            route = reverse(ROUTES.SEARCH, { searchTerm: encodeURIComponent(value) });
+        }
+        return history.push(route);
     };
 
-    render() {
-        if (this.state.redirect) {
-            this.setState({
-                redirect: false,
-                value: ''
-            });
+    return (
+        <Form className="mt-2 mt-md-0 mr-3 search-box mb-2 mb-md-0" inline onSubmit={handleSubmit} style={{ minWidth: 57 }}>
+            <InputGroup>
+                <Input
+                    placeholder={props.placeholder}
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Search ORKG"
+                    aria-describedby="button-main-search"
+                />
 
-            return <Redirect to={reverse(ROUTES.SEARCH, { searchTerm: this.state.value })} />;
-        }
-
-        return (
-            <form className="form-inline mt-2 mt-md-0 mr-3 search-box mb-2 mb-md-0" onSubmit={this.handleSubmit} style={{ minWidth: 57 }}>
-                <div className="input-group">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder={this.props.placeholder}
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        aria-label="Search ORKG"
-                        aria-describedby="button-main-search"
-                    />
-
-                    <div className="input-group-append">
-                        <button id="button-main-search" className="btn btn-outline-secondary pl-2 pr-2 search-icon" type="submit">
-                            <FontAwesomeIcon icon={faSearch} />
-                        </button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
-}
-
-SearchForm.propTypes = {
-    location: PropTypes.object.isRequired,
-    placeholder: PropTypes.string.isRequired,
-    match: PropTypes.shape({
-        params: PropTypes.shape({
-            searchTerm: PropTypes.string
-        }).isRequired
-    }).isRequired
+                <InputGroupAddon addonType="append">
+                    <Button id="button-main-search" className="pl-2 pr-2 search-icon" type="submit">
+                        <Icon icon={faSearch} />
+                    </Button>
+                </InputGroupAddon>
+            </InputGroup>
+        </Form>
+    );
 };
 
-export default withRouter(SearchForm);
+SearchForm.propTypes = {
+    placeholder: PropTypes.string.isRequired
+};
+
+export default SearchForm;
