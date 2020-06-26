83236484a376742f152155c90aedcec73e1f59ce
Kheir Eddine Farfar <kfarfar@gmail.com>
2020-06-26 11:10:56 +0200

    change tooltip text

--- src/components/StatementBrowser/AddValue/AddValueTemplate.js
+++ src/components/StatementBrowser/AddValue/AddValueTemplate.js
@@ -288,7 +288,7 @@ export default function AddValueTemplate(props) {
                                 }}
                             >
                                 {disabledCreate ? (
-                                    <Tippy content="Please use existing research problem." arrow={true}>
+                                    <Tippy content="Please use the existing research problem that has this label." arrow={true}>
                                         <span>Create</span>
                                     </Tippy>
                                 ) : (
