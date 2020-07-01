e2fe2552efc1c3e54f3d34ca6f1a9fb567bd009a
Allard Oelen <oelen@l3s.de>
2020-07-01 12:30:20 +0200

    Refactor SuggestedTemplates component

--- src/components/StatementBrowser/SuggestedTemplates/SuggestedTemplates.js
+++ src/components/StatementBrowser/SuggestedTemplates/SuggestedTemplates.js
@@ -1,80 +1,67 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentLoader from 'react-content-loader';
 import { getStatementsByObjectAndPredicate, getParentResearchFields } from 'network';
 import Tooltip from 'components/Utils/Tooltip';
 import AddTemplateButton from 'components/StatementBrowser/AddTemplateButton/AddTemplateButton';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { isEqual, difference, flattenDepth } from 'lodash';
+import { flattenDepth, uniqBy } from 'lodash';
 import PropTypes from 'prop-types';
 import { FormGroup, Label, UncontrolledAlert } from 'reactstrap';
 
-// https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
-function usePrevious(value) {
-    const ref = useRef();
-    useEffect(() => {
-        ref.current = value;
-    });
-    return ref.current;
-}
-
 export default function SuggestedTemplates(props) {
-    const [isTemplatesLoading, setIsTemplatesLoading] = useState(false);
-    const [isTemplatesFailedLoading, setIsTemplatesFailedLoading] = useState(false);
-    const [templates, setTemplates] = useState([]);
+    const [problemTemplates, setProblemTemplates] = useState([]);
+    const [fieldTemplates, setFieldTemplates] = useState([]);
+    const [loadedProblems, setLoadedProblems] = useState([]);
+    const [loadingFieldTemplates, setLoadingFieldTemplates] = useState({ isLoading: false, failed: false });
+    const [loadingProblemTemplates, setLoadingProblemTemplates] = useState({ isLoading: false, failed: false });
 
-    // how-to-compare-oldvalues-and-newvalues-on-react-hooks-useeffect
-    // https://stackoverflow.com/questions/53446020/how-to-compare-oldvalues-and-newvalues-on-react-hooks-useeffect
-    const prevTarget = usePrevious({ researchField: props.researchField, researchProblems: props.researchProblems });
-
+    /**
+     * Fetch templates for research fields
+     */
     useEffect(() => {
-        const loadResearchFieldTemplates = () => {
-            setIsTemplatesLoading(true);
-            setIsTemplatesFailedLoading(false);
-            // Load templates of the research field and parents and the research problems
-            getParentResearchFields(props.researchField).then(parents => {
-                Promise.all([
-                    ...parents.map(rf => getTemplatesOfResourceId(rf.id, process.env.REACT_APP_TEMPLATE_OF_RESEARCH_FIELD)),
-                    ...props.researchProblems.map(rp => getTemplatesOfResourceId(rp, process.env.REACT_APP_TEMPLATE_OF_RESEARCH_PROBLEM))
-                ])
-                    .then(templates => {
-                        setTemplates(templates.flat());
-                        setIsTemplatesLoading(false);
-                        setIsTemplatesFailedLoading(false);
-                    })
-                    .catch(e => {
-                        setTemplates([]);
-                        setIsTemplatesLoading(false);
-                        setIsTemplatesFailedLoading(true);
-                    });
-            });
-        };
-        if (prevTarget && !isEqual(prevTarget.researchProblems, props.researchProblems)) {
-            // update the list of templates when the list research problem changes
-            setIsTemplatesLoading(true);
-            setIsTemplatesFailedLoading(false);
-            const toRemove = difference(prevTarget.researchProblems, props.researchProblems);
-            const toAdd = difference(props.researchProblems, prevTarget.researchProblemss);
-            let newTemplates = templates;
-            if (toRemove.length > 0) {
-                newTemplates = newTemplates.filter(template => {
-                    return !toRemove.includes(template.source.id);
+        setLoadingFieldTemplates({ isLoading: true, failed: false });
+
+        getParentResearchFields(props.researchField).then(parents => {
+            Promise.all([...parents.map(rf => getTemplatesOfResourceId(rf.id, process.env.REACT_APP_TEMPLATE_OF_RESEARCH_FIELD))])
+                .then(templates => {
+                    setFieldTemplates(templates.flat());
+                    setLoadingFieldTemplates({ isLoading: false, failed: false });
+                })
+                .catch(e => {
+                    setFieldTemplates([]);
+                    setLoadingFieldTemplates({ isLoading: false, failed: true });
                 });
-            }
-            const fetchTemplatesPromise = toAdd.map(problemId => {
-                return getTemplatesOfResourceId(problemId, process.env.REACT_APP_TEMPLATE_OF_RESEARCH_PROBLEM);
+        });
+    }, [props.researchField]);
+
+    /**
+     * Fetch templates for research problems
+     */
+    useEffect(() => {
+        setLoadingProblemTemplates({ isLoading: true, failed: false });
+
+        // fetch templates for problems there aren't loaded yet
+        const problemPromises = props.researchProblems
+            .filter(problem => !loadedProblems.includes(problem))
+            .map(problem => getTemplatesOfResourceId(problem, process.env.REACT_APP_TEMPLATE_OF_RESEARCH_PROBLEM));
+
+        Promise.all(problemPromises)
+            .then(addTemplates => {
+                // flatten depth created by promise all
+                addTemplates = flattenDepth(addTemplates, 2);
+                setProblemTemplates(state => [
+                    ...addTemplates,
+                    ...state.filter(template => {
+                        return props.researchProblems.includes(template.source.id);
+                    })
+                ]);
+                setLoadedProblems(props.researchProblems);
+                setLoadingProblemTemplates({ isLoading: false, failed: false });
+            })
+            .catch(e => {
+                setProblemTemplates([]);
+                setLoadingProblemTemplates({ isLoading: false, failed: true });
             });
-            Promise.all(fetchTemplatesPromise).then(addtemplates => {
-                setTemplates([...newTemplates, ...flattenDepth(addtemplates, 2)]);
-                setIsTemplatesLoading(false);
-                setIsTemplatesFailedLoading(false);
-            });
-        }
-        if (!prevTarget || prevTarget.researchField !== props.researchField) {
-            loadResearchFieldTemplates();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.researchField, props.researchProblems, props.selectedResource]);
+    }, [props.researchProblems, loadedProblems]);
 
     /**
      * Fetch the templates of a resource
@@ -92,61 +79,48 @@ export default function SuggestedTemplates(props) {
         });
     };
 
-    const uniqueTemplates = [];
-    templates.forEach(obj => {
-        if (!uniqueTemplates.some(o => o.id === obj.id)) {
-            uniqueTemplates.push({ ...obj });
-        }
-    });
+    const templatesUnique = uniqBy([...problemTemplates, ...fieldTemplates], 'id');
+    const isLoading = loadingFieldTemplates.isLoading || loadingProblemTemplates.isLoading;
+    const loadingFailed = loadingFieldTemplates.failed && loadingProblemTemplates.failed;
+
+    if (props.disabled) {
+        return <UncontrolledAlert color="info">A shared resource cannot be edited directly</UncontrolledAlert>;
+    }
 
     return (
-        <div>
-            {isTemplatesLoading ? (
-                <>
-                    <ContentLoader height={90} speed={2} primaryColor="#f3f3f3" secondaryColor="#ecebeb">
-                        <rect x="0" y="0" width="90" height="12" />
-                        <rect x="0" y="18" rx="7" ry="7" width="55" height="15" />
-                        <rect x="60" y="18" rx="7" ry="7" width="55" height="15" />
-                        <rect x="120" y="18" rx="7" ry="7" width="55" height="15" />
-                        <rect x="180" y="18" rx="7" ry="7" width="55" height="15" />
-                    </ContentLoader>
-                </>
-            ) : !isTemplatesFailedLoading ? (
-                <FormGroup>
-                    {uniqueTemplates.length > 0 && (
-                        <>
-                            <Label>
-                                <Tooltip message="Select a template to use it in your data">Use template</Tooltip>
-                            </Label>
-                            <div>
-                                {isTemplatesLoading && (
-                                    <>
-                                        <Icon icon={faSpinner} spin /> Loading templates.
-                                    </>
-                                )}
-                                {!isTemplatesLoading && uniqueTemplates.length === 0 && <>No template found.</>}
-                                {!isTemplatesLoading && uniqueTemplates.length > 0 && (
-                                    <>
-                                        {uniqueTemplates.map(t => (
-                                            <AddTemplateButton
-                                                key={`t${t.id}`}
-                                                id={t.id}
-                                                label={t.label}
-                                                source={t.source}
-                                                selectedResource={props.selectedResource}
-                                                syncBackend={props.syncBackend}
-                                            />
-                                        ))}
-                                    </>
-                                )}
-                            </div>
-                        </>
-                    )}
-                </FormGroup>
-            ) : (
-                <UncontrolledAlert color="info">Failed to load templates!</UncontrolledAlert>
+        <>
+            {isLoading && (
+                <ContentLoader height={50} speed={2} primaryColor="#f3f3f3" secondaryColor="#ecebeb">
+                    <rect x="0" y="18" rx="7" ry="7" width="55" height="18" />
+                    <rect x="60" y="18" rx="7" ry="7" width="55" height="18" />
+                    <rect x="120" y="18" rx="7" ry="7" width="55" height="18" />
+                    <rect x="180" y="18" rx="7" ry="7" width="55" height="18" />
+                </ContentLoader>
             )}
-        </div>
+
+            {!isLoading && loadingFailed && <UncontrolledAlert color="info">Failed to load templates</UncontrolledAlert>}
+
+            {!isLoading && !loadingFailed && templatesUnique.length > 0 && (
+                <FormGroup>
+                    <Label>
+                        <Tooltip message="Select a template to use it in your data">Use template</Tooltip>
+                    </Label>
+
+                    <div>
+                        {templatesUnique.map(template => (
+                            <AddTemplateButton
+                                key={`t${template.id}`}
+                                id={template.id}
+                                label={template.label + template.id}
+                                source={template.source}
+                                selectedResource={props.selectedResource}
+                                syncBackend={props.syncBackend}
+                            />
+                        ))}
+                    </div>
+                </FormGroup>
+            )}
+        </>
     );
 }
 
@@ -154,9 +128,11 @@ SuggestedTemplates.propTypes = {
     syncBackend: PropTypes.bool.isRequired,
     researchProblems: PropTypes.array.isRequired,
     researchField: PropTypes.string.isRequired,
-    selectedResource: PropTypes.string.isRequired
+    selectedResource: PropTypes.string.isRequired,
+    disabled: PropTypes.bool.isRequired
 };
 
 SuggestedTemplates.defaultProps = {
-    syncBackend: false
+    syncBackend: false,
+    disabled: false
 };
