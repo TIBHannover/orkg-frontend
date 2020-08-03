89b21fe4b17b7e339ebbf8c3c797335ffe1f6a14
Allard Oelen <oelen@l3s.de>
2020-08-03 12:25:48 +0200

    style(ResearchProblem): show contribution label in papers list

--- src/components/PaperCard/PaperCard.js
+++ src/components/PaperCard/PaperCard.js
@@ -44,9 +44,12 @@ const PaperCard = props => {
                 )}
                 <Col>
                     {props.contribution && (
-                        <Link to={reverse(ROUTES.VIEW_PAPER, { resourceId: props.paper.id, contributionId: props.contribution.id })}>
-                            {props.paper.title ? props.paper.title : <em>No title</em>}
-                        </Link>
+                        <>
+                            <Link to={reverse(ROUTES.VIEW_PAPER, { resourceId: props.paper.id, contributionId: props.contribution.id })}>
+                                {props.paper.title ? props.paper.title : <em>No title</em>}
+                            </Link>{' '}
+                            - <span className="text-muted">{props.contribution.title}</span>
+                        </>
                     )}
                     {!props.contribution && (
                         <Link to={reverse(ROUTES.VIEW_PAPER, { resourceId: props.paper.id })}>
@@ -67,6 +70,7 @@ const PaperCard = props => {
                         {props.paper.publicationYear}
                     </small>
                 </Col>
+                {console.log(props)}
                 {props.contribution && (
                     <div className="options mr-2">
                         <AddToComparison
