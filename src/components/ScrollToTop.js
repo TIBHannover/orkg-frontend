4acfdf7a56c24f0d01c6a81378a53aaa69eb636a
Allard Oelen <oelen@l3s.de>
2020-08-07 11:48:18 +0200

    replace hardcoded url

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,15 +1,19 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ROUTES from 'constants/routes.js';
+import { match } from 'path-to-regexp';
 
 /* Srolls browser window to top when new page is visited,
 but preserves scroll position when previous page is visited */
 class ScrollToTop extends Component {
     componentDidUpdate(prevProps) {
         if (this.props.location !== prevProps.location) {
-            const isViewPaper_contributionChange =
-                this.props.location.pathname.startsWith('/paper/') && prevProps.location.pathname.startsWith('/paper/');
-            if (!isViewPaper_contributionChange) {
+            const matchViewPaper = match(ROUTES.VIEW_PAPER);
+            const contributionChange = matchViewPaper(this.props.location.pathname) && matchViewPaper(prevProps.location.pathname);
+
+            // don't scroll to top when only the contribution changes on the view paper page
+            if (!contributionChange) {
                 window.scrollTo(0, 0);
             }
         }
