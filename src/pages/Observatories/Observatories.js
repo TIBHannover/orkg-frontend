509519880753870db0f9f17d0b3bba69918503ea
mharis111 <38453478+mharis111@users.noreply.github.com>
2020-09-03 16:33:06 +0200

    Update Observatories.js

--- src/pages/Observatories/Observatories.js
+++ src/pages/Observatories/Observatories.js
@@ -1,31 +1,113 @@
 import React from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import Typography from '@material-ui/core/Typography';
+import { observatoriesUrl, submitGetRequest } from 'network';
+import { Container } from 'reactstrap';
+import ObservatoryCard from 'components/ObservatoryCard/ObservatoryCard';
 import { withStyles } from '@material-ui/core/styles';
 
 class Observatories extends React.PureComponent {
-    state = { activeIndex: 0 };
+    //state = { activeIndex: 0 };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            observatories: [],
+            isNextPageLoading: false,
+            activeIndex: 0
+        };
+    }
+
+    componentDidMount() {
+        document.title = 'Observatories - ORKG';
+        this.loadObservatories();
+    }
+
+    loadObservatories = () => {
+        this.setState({ isNextPageLoading: true });
+        submitGetRequest(observatoriesUrl)
+            .then(observatories => {
+                const g = this.groupBy(observatories, 'researchField');
+                if (observatories.length > 0) {
+                    this.setState({
+                        observatories: g,
+                        isNextPageLoading: false
+                    });
+                } else {
+                    this.setState({
+                        isNextPageLoading: false
+                    });
+                }
+            })
+            .catch(error => {
+                this.setState({
+                    isNextPageLoading: false
+                });
+            });
+    };
+
+    groupBy = (array, key) => {
+        return array.reduce((result, currentValue) => {
+            (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
+            return result;
+        }, {});
+    };
 
     handleChange = (_, activeIndex) => this.setState({ activeIndex });
     render() {
         const { activeIndex } = this.state;
         return (
-            <div
-                style={{
-                    display: 'flex'
-                }}
-            >
-                <VerticalTabs value={activeIndex} onChange={this.handleChange}>
+            <>
+                <Container className="p-0">
+                    <h1 className="h4 mt-4 mb-4">View all observatories </h1>
+                </Container>
+                <Container className="box rounded pt-4 pb-4 pl-5 pr-5 clearfix">
+                    {console.log(activeIndex)}
+                    <VerticalTabs value={activeIndex} onChange={this.handleChange}>
+                        {Object.keys(this.state.observatories).map((rf, key) => {
+                            return <MyTab key={`c${key}`} label={rf === 'null' || '' ? 'Others' : rf} />;
+                        })}
+                    </VerticalTabs>
+                    {/* <Container> */}
+                    <div className="mt-3 row justify-content-center">
+                        {Object.keys(this.state.observatories).map((rf, key) => {
+                            return (
+                                activeIndex === key && (
+                                    <>
+                                        {/* <div className="mt-3 row justify-content-center"> */}
+                                        {this.state.observatories[rf].map(observatory => {
+                                            //  return <TabContainer key={`c${key}`}>{observatory.name}</TabContainer>;
+                                            return <ObservatoryCard key={observatory.id} observatory={observatory} />;
+                                        })}
+                                        {/* </div> */}
+                                    </>
+                                )
+                            );
+                        })}
+                    </div>
+                    {/* </Container> */}
+                    {/* <VerticalTabs value={activeIndex} onChange={this.handleChange}>
                     <MyTab label="item one" />
                     <MyTab label="item two" />
                     <MyTab label="item three" />
-                </VerticalTabs>
+                </VerticalTabs> */}
 
-                {activeIndex === 0 && <TabContainer>Item One</TabContainer>}
+                    {/* {activeIndex === 0 && <TabContainer>Item One</TabContainer>}
                 {activeIndex === 1 && <TabContainer>Item Two</TabContainer>}
-                {activeIndex === 2 && <TabContainer>Item Three</TabContainer>}
-            </div>
+                {activeIndex === 2 && <TabContainer>Item Three</TabContainer>} */}
+                    {this.state.observatories.length === 0 && !this.state.isNextPageLoading && (
+                        <div className="text-center mt-4 mb-4">No observatories yet!</div>
+                    )}
+                    {this.state.isNextPageLoading && (
+                        <div className="text-center mt-4 mb-4">
+                            <Icon icon={faSpinner} spin /> Loading
+                        </div>
+                    )}
+                </Container>
+            </>
         );
     }
 }
