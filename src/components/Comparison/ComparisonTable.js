9d29185b2fd9f07e38aaf669d51aa85db3954b68
Allard Oelen <oelen@l3s.de>
2021-01-12 15:04:34 +0100

    style(Comparison): move publication year to front in comparison header

--- src/components/Comparison/ComparisonTable.js
+++ src/components/Comparison/ComparisonTable.js
@@ -262,7 +262,8 @@ class ComparisonTable extends Component {
                                                                   </Link>
                                                                   <br />
                                                                   <Contribution>
-                                                                      {contribution.contributionLabel} {contribution.year && `- ${contribution.year}`}
+                                                                      {contribution.year && `${contribution.year} - `}{' '}
+                                                                      {contribution.contributionLabel}
                                                                   </Contribution>
                                                               </ItemHeaderInner>
 
