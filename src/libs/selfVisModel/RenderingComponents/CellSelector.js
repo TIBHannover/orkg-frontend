a0f8b87b711f4304bcb239accebed32055966ab8
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-01-26 10:57:52 +0100

    convert CellSelector to functional component

--- src/libs/selfVisModel/RenderingComponents/CellSelector.js
+++ src/libs/selfVisModel/RenderingComponents/CellSelector.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Alert, ButtonGroup, Button } from 'reactstrap';
 import SelfVisDataModel from 'libs/selfVisModel/SelfVisDataModel';
 import CellRenderer from './CellRenderer';
@@ -6,28 +6,25 @@ import DropDownMapperSelector from './DropdownMapperSelector';
 import CheckboxSelector from './CheckBoxSelector';
 import PropTypes from 'prop-types';
 
-export default class CellSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.selfVisModel = new SelfVisDataModel(); // this access the instance of the data (its a singleton)
-    }
+const CellSelector = props => {
+    const [selfVisModel] = useState(new SelfVisDataModel());
 
     /** some data handlers **/
-    toggleCheckboxForCol = (id, value) => {
+    const toggleCheckboxForCol = (id, value) => {
         // this is the correct id for the init from the propertyAnchors
-        this.setFullColumnSelection(id, value);
+        setFullColumnSelection(id, value);
     };
 
     /** Rendering functions for the frame (headers for rows and cols ) **/
-    createTable = () => {
+    const createTable = () => {
         // we have a rootItem which is a split between properties and contributions
         /* [root] [property], [property]
           cId  [ value 0,0] value [1,0]...]
           cId  [ value 0,1] value [1,1]...]      
         */
 
-        const renderingDimX = this.selfVisModel.mrrModel.propertyAnchors.length + 1;
-        const renderingDimY = this.selfVisModel.mrrModel.contributionAnchors.length + 1;
+        const renderingDimX = selfVisModel.mrrModel.propertyAnchors.length + 1;
+        const renderingDimY = selfVisModel.mrrModel.contributionAnchors.length + 1;
 
         const itemsToRender = [];
 
@@ -65,14 +62,14 @@ export default class CellSelector extends Component {
                                         >
                                             <CheckboxSelector
                                                 label=""
-                                                initializedValue={this.selfVisModel.mrrModel.propertyAnchors[j - 1]}
-                                                handleCheckboxChange={v => this.toggleCheckboxForCol(j - 1, v)}
+                                                initializedValue={selfVisModel.mrrModel.propertyAnchors[j - 1]}
+                                                handleCheckboxChange={v => toggleCheckboxForCol(j - 1, v)}
                                                 cbx_id={'useValue+' + i + '_' + j}
                                                 key={'useValue+' + i + '_' + j}
                                             />
                                         </div>
                                     </Button>
-                                    <DropDownMapperSelector key={keyVal + 'dropdown'} data={this.selfVisModel.mrrModel.propertyAnchors[j - 1]} />
+                                    <DropDownMapperSelector key={keyVal + 'dropdown'} data={selfVisModel.mrrModel.propertyAnchors[j - 1]} />
                                 </ButtonGroup>
                             </CellRenderer>
                         );
@@ -88,13 +85,13 @@ export default class CellSelector extends Component {
                     rowArray.push(<CellRenderer key={keyVal} type="metaNode" data={null} />);
                 }
                 if (i === 0 && j !== 0) {
-                    rowArray.push(<CellRenderer key={keyVal} type="property" data={this.selfVisModel.mrrModel.propertyAnchors[j - 1]} />);
+                    rowArray.push(<CellRenderer key={keyVal} type="property" data={selfVisModel.mrrModel.propertyAnchors[j - 1]} />);
                 }
                 if (i > 0 && j === 0) {
-                    rowArray.push(<CellRenderer key={keyVal} type="contribution" data={this.selfVisModel.mrrModel.contributionAnchors[i - 1]} />);
+                    rowArray.push(<CellRenderer key={keyVal} type="contribution" data={selfVisModel.mrrModel.contributionAnchors[i - 1]} />);
                 }
                 if (i > 0 && j !== 0) {
-                    rowArray.push(<CellRenderer key={keyVal} type="value" data={this.selfVisModel.modelAccess.getItem(i - 1, j - 1)} />);
+                    rowArray.push(<CellRenderer key={keyVal} type="value" data={selfVisModel.modelAccess.getItem(i - 1, j - 1)} />);
                 }
             }
 
@@ -109,56 +106,52 @@ export default class CellSelector extends Component {
         // we will use a similar styles as done in the table renderer of the comparison.
     };
 
-    /** Preparations for mouse are selection:  Cell interactions for single entries or rows & cols or full **/
-    setSelectionFlagInData = (row, col) => {
+    /** Preparations for mouse are selection:  Cell interactions for single entries or rows & cols or full 
+    const setSelectionFlagInData = (row, col) => {
         // check the source of origin;
         console.log('TODO', 'access data and set selected Flag in the cell ');
     };
 
-    removeSelectionFlagInData = (row, col) => {
+    const removeSelectionFlagInData = (row, col) => {
         // check the source of origin;
         console.log('TODO', 'access data and remove selected Flag in the cell ');
     };
 
-    clearSelectionFlagInData = () => {
+    const clearSelectionFlagInData = () => {
         // check the source of origin;
         console.log('TODO', 'remove all selection flags');
     };
 
-    setFullRowSelection = (rowId, value) => {
+    const setFullRowSelection = (rowId, value) => {
         // check the source of origin;
         console.log('TODO', 'Set all flags to value for a specific row');
     };
+    **/
 
-    setFullColumnSelection = (columnId, value) => {
-        const colItems = this.selfVisModel.modelAccess.getCol(columnId);
-        this.selfVisModel.mrrModel.propertyAnchors[columnId].setSelectedColumn(value);
+    const setFullColumnSelection = (columnId, value) => {
+        const colItems = selfVisModel.modelAccess.getCol(columnId);
+        selfVisModel.mrrModel.propertyAnchors[columnId].setSelectedColumn(value);
 
-        for (let i = 0; i < this.selfVisModel.mrrModel.contributionAnchors.length; i++) {
-            this.selfVisModel.mrrModel.contributionAnchors[i].setSelectedRow(true);
+        for (let i = 0; i < selfVisModel.mrrModel.contributionAnchors.length; i++) {
+            selfVisModel.mrrModel.contributionAnchors[i].setSelectedRow(true);
             colItems[i].setItemSelected(value);
         }
     };
 
-    /** component rendering entrance point **/
-    render() {
-        return (
-            <div className="pt-2">
-                <Alert color="info" fade={false}>
-                    Select cells for the visualization and map them to types
-                </Alert>
+    return (
+        <div className="pt-2">
+            <Alert color="info" fade={false}>
+                Select cells for the visualization and map them to types
+            </Alert>
 
-                {this.props.isLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <div style={{ height: this.props.height + 'px', overflow: 'auto' }}>{this.createTable()} </div>
-                )}
-            </div>
-        );
-    }
-}
+            {props.isLoading ? <div>Loading...</div> : <div style={{ height: props.height + 'px', overflow: 'auto' }}>{createTable()} </div>}
+        </div>
+    );
+};
 
 CellSelector.propTypes = {
     isLoading: PropTypes.bool,
     height: PropTypes.number
 };
+
+export default CellSelector;
