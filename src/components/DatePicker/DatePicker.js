ca7db5d87d3993ba0c7ceb7a032b0c43af9b6e3d
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-02-15 18:12:06 +0100

    make renderCustomHeader of DatePicker optional

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -4,81 +4,90 @@ import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { range } from 'utils';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
-export default function DatePicker(props) {
+function DatePicker(props) {
     const years = range(1900, moment().year() + 1).reverse();
     const months = moment.months();
     return (
         <BaseDatePicker
             {...props}
-            className="form-control"
-            renderCustomHeader={({
-                date,
-                changeYear,
-                changeMonth,
-                decreaseMonth,
-                increaseMonth,
-                prevMonthButtonDisabled,
-                nextMonthButtonDisabled
-            }) => (
-                <div
-                    style={{
-                        margin: 10,
-                        display: 'flex',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <Button
-                        style={{ padding: '2px 8px', borderBottomRightRadius: '0', borderTopRightRadius: '0' }}
-                        size="sm"
-                        onClick={decreaseMonth}
-                        disabled={prevMonthButtonDisabled}
-                    >
-                        <Icon icon={faChevronLeft} />
-                    </Button>
-                    <Input
-                        style={{ borderRadius: '0' }}
-                        size="sm"
-                        type="select"
-                        name="select"
-                        value={moment(date, 'YYYY-MM-DD').year()}
-                        onChange={({ target: { value } }) => changeYear(value)}
-                        onBlur={() => null}
-                    >
-                        {years.map(option => (
-                            <option key={option} value={option}>
-                                {option}
-                            </option>
-                        ))}
-                    </Input>
-                    <Input
-                        style={{ borderRadius: '0' }}
-                        size="sm"
-                        type="select"
-                        name="select"
-                        value={moment(date, 'YYYY-MM-DD').month()}
-                        onChange={({ target: { value } }) => {
-                            changeMonth(value);
-                        }}
-                        onBlur={() => null}
-                    >
-                        {months.map((option, index) => (
-                            <option key={index} value={index}>
-                                {option}
-                            </option>
-                        ))}
-                    </Input>
+            className={`form-control ${props.cssClasses ? props.cssClasses : ''}`}
+            renderCustomHeader={
+                props.renderCustomHeader
+                    ? ({ date, changeYear, changeMonth, decreaseMonth, increaseMonth, prevMonthButtonDisabled, nextMonthButtonDisabled }) => (
+                          <div
+                              style={{
+                                  margin: 10,
+                                  display: 'flex',
+                                  justifyContent: 'center'
+                              }}
+                          >
+                              <Button
+                                  style={{ padding: '2px 8px', borderBottomRightRadius: '0', borderTopRightRadius: '0' }}
+                                  size="sm"
+                                  onClick={decreaseMonth}
+                                  disabled={prevMonthButtonDisabled}
+                              >
+                                  <Icon icon={faChevronLeft} />
+                              </Button>
+                              <Input
+                                  style={{ borderRadius: '0' }}
+                                  bsSize="sm"
+                                  type="select"
+                                  name="select"
+                                  value={moment(date, 'YYYY-MM-DD').year()}
+                                  onChange={({ target: { value } }) => changeYear(value)}
+                                  onBlur={() => null}
+                              >
+                                  {years.map(option => (
+                                      <option key={option} value={option}>
+                                          {option}
+                                      </option>
+                                  ))}
+                              </Input>
+                              <Input
+                                  style={{ borderRadius: '0' }}
+                                  bsSize="sm"
+                                  type="select"
+                                  name="select"
+                                  value={moment(date, 'YYYY-MM-DD').month()}
+                                  onChange={({ target: { value } }) => {
+                                      changeMonth(value);
+                                  }}
+                                  onBlur={() => null}
+                              >
+                                  {months.map((option, index) => (
+                                      <option key={index} value={index}>
+                                          {option}
+                                      </option>
+                                  ))}
+                              </Input>
 
-                    <Button
-                        style={{ padding: '2px 8px', borderBottomLeftRadius: '0', borderTopLeftRadius: '0' }}
-                        size="sm"
-                        onClick={increaseMonth}
-                        disabled={nextMonthButtonDisabled}
-                    >
-                        <Icon icon={faChevronRight} />
-                    </Button>
-                </div>
-            )}
+                              <Button
+                                  style={{ padding: '2px 8px', borderBottomLeftRadius: '0', borderTopLeftRadius: '0' }}
+                                  size="sm"
+                                  onClick={increaseMonth}
+                                  disabled={nextMonthButtonDisabled}
+                              >
+                                  <Icon icon={faChevronRight} />
+                              </Button>
+                          </div>
+                      )
+                    : undefined
+            }
         />
     );
 }
+
+DatePicker.propTypes = {
+    cssClasses: PropTypes.string,
+    renderCustomHeader: PropTypes.bool
+};
+
+DatePicker.defaultProps = {
+    cssClasses: '',
+    renderCustomHeader: true
+};
+
+export default DatePicker;
