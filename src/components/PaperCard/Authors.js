7fd9cf08af8d7858ecf4c2c60d963061f7e2dc58
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-02-24 16:43:17 +0100

    refactor: Paper card, max number of authors

---
+++ src/components/PaperCard/Authors.js
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+
+const Authors = ({ authors, maxAuthors }) => {
+    return (
+        <>
+            {authors && authors.length > 0 && (
+                <>
+                    <Icon size="sm" icon={faUser} />{' '}
+                    {authors
+                        .slice(0, maxAuthors)
+                        .map(a => a.label)
+                        .join(', ')}
+                    {authors.length > maxAuthors && ' et al.'}
+                </>
+            )}
+        </>
+    );
+};
+
+Authors.propTypes = {
+    authors: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    maxAuthors: PropTypes.number
+};
+
+Authors.defaultProps = {
+    maxAuthors: 5
+};
+
+export default Authors;
