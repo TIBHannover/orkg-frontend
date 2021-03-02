ca8526bc260a13ce2bef121369d1b6fc53f916ad
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-03-02 10:07:25 +0100

    Show observatory and organization in profile page

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,19 +1,54 @@
 import { useState, useEffect } from 'react';
 import { Container, Row } from 'reactstrap';
-import PropTypes from 'prop-types';
 import { getUserInformationById } from 'services/backend/users';
 import Items from 'components/UserProfile/Items';
 import { getObservatoryById } from 'services/backend/observatories';
 import { getOrganization } from 'services/backend/organizations';
 import NotFound from 'pages/NotFound';
+import ContentLoader from 'react-content-loader';
 import { useSelector } from 'react-redux';
 import Gravatar from 'react-gravatar';
 import styled from 'styled-components';
 import { CLASSES } from 'constants/graphSettings';
+import ROUTES from 'constants/routes';
+import { reverse } from 'named-urls';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const StyledGravatar = styled(Gravatar)`
     border: 3px solid ${props => props.theme.avatarBorderColor};
 `;
+
+const StyledOrganizationCard = styled.div`
+    border: 0;
+    text-align: center;
+    .logoContainer {
+        position: relative;
+        display: block;
+
+        &::before {
+            // for aspect ratio
+            content: '';
+            display: block;
+            padding-bottom: 100px;
+        }
+        img {
+            position: absolute;
+            max-width: 100%;
+            max-height: 100px;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+        &:active,
+        &:focus {
+            outline: 0;
+            border: none;
+            -moz-outline-style: none;
+        }
+    }
+`;
+
 /*
 const StyledActivity = styled.div`
     border-left: 3px solid #e9ebf2;
@@ -48,6 +83,7 @@ const UserProfile = props => {
     const [userData, setUserData] = useState('');
     const [observatoryData, setObservatoryData] = useState(null);
     const [organizationData, setOrganizationData] = useState(null);
+    const [isLoadingUserData, setIsLoadingUserData] = useState(false);
     const [notFound, setNotFound] = useState(false);
     const userId = props.match.params.userId;
     const currentUserId = useSelector(state => state.auth.user?.id);
@@ -55,32 +91,50 @@ const UserProfile = props => {
     useEffect(() => {
         const getUserInformation = async () => {
             setNotFound(false);
+            setIsLoadingUserData(true);
+            getUserInformationById(userId)
+                .then(userData => {
+                    if (userData.observatory_id || userData.organization_id) {
+                        const promises = [];
+                        if (userData.organization_id) {
+                            const promise1 = getOrganization(userData.organization_id);
+                            promises.push(promise1);
+                        }
+                        if (userData.observatory_id) {
+                            const promise2 = getObservatoryById(userData.observatory_id);
+                            promises.push(promise2);
+                        }
 
-            try {
-                const userData = await getUserInformationById(userId);
-                setUserData(userData);
-                if (userData.observatory_id) {
-                    getObservatoryById(userData.observatory_id)
-                        .then(observatory => {
-                            setObservatoryData(observatory);
-                        })
-                        .catch(error => {
-                            setObservatoryData(null);
-                        });
-                }
-
-                if (userData.organization_id) {
-                    getOrganization(userData.organization_id)
-                        .then(organization => {
-                            setOrganizationData(organization);
-                        })
-                        .catch(err => {
-                            setOrganizationData(null);
-                        });
-                }
-            } catch (e) {
-                setNotFound(true);
-            }
+                        Promise.all(promises)
+                            .then(obsOrgData => {
+                                console.log(obsOrgData);
+                                if (userData.organization_id) {
+                                    setOrganizationData(obsOrgData[0]);
+                                }
+                                if (userData.observatory_id) {
+                                    setObservatoryData(obsOrgData[1]);
+                                }
+                                setUserData(userData);
+                                setIsLoadingUserData(false);
+                            })
+                            .catch(e => {
+                                if (userData.organization_id) {
+                                    setOrganizationData(null);
+                                }
+                                if (userData.observatory_id) {
+                                    setObservatoryData(null);
+                                }
+                                setUserData(userData);
+                                setIsLoadingUserData(false);
+                            });
+                    } else {
+                        setUserData(userData);
+                        setIsLoadingUserData(false);
+                    }
+                })
+                .catch(e => {
+                    setNotFound(true);
+                });
         };
 
         getUserInformation();
@@ -93,26 +147,60 @@ const UserProfile = props => {
     return (
         <>
             <Container>
-                <Row>
-                    <div className="col-2 text-center">
-                        <StyledGravatar className="rounded-circle" email={userData?.email ?? 'example@example.com'} size={100} id="TooltipExample" />
+                {!isLoadingUserData && (
+                    <Row>
+                        <div className="col-2 text-center d-flex align-items-center justify-content-center">
+                            <StyledGravatar
+                                className="rounded-circle"
+                                email={userData?.email ?? 'example@example.com'}
+                                size={100}
+                                id="TooltipExample"
+                            />
+                        </div>
+                        <div className="col-10 box rounded p-4">
+                            <div className="row">
+                                <div className="col-8">
+                                    <h2 className="h3">{userData.display_name}</h2>
+                                    {observatoryData && (
+                                        <div className="mt-3">
+                                            <b className="d-block">Member of the observatory</b>
+                                            <Link to={reverse(ROUTES.OBSERVATORY, { id: observatoryData?.id })} className="text-center">
+                                                {observatoryData?.name}
+                                            </Link>
+                                        </div>
+                                    )}
+                                </div>
+                                <div className="col-4">
+                                    {organizationData && (
+                                        <StyledOrganizationCard>
+                                            <Link className="logoContainer" to={reverse(ROUTES.ORGANIZATION, { id: organizationData.id })}>
+                                                <img className="mx-auto p-2" src={organizationData.logo} alt={`${organizationData.name} logo`} />
+                                            </Link>
+                                            <Link to={reverse(ROUTES.ORGANIZATION, { id: organizationData.id })}>{organizationData?.name}</Link>
+                                        </StyledOrganizationCard>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </Row>
+                )}
+                {isLoadingUserData && (
+                    <div className="mt-4 ml-3">
+                        <ContentLoader
+                            speed={2}
+                            width={500}
+                            height={100}
+                            viewBox="0 0 500 100"
+                            style={{ width: '100% !important' }}
+                            backgroundColor="#f3f3f3"
+                            foregroundColor="#ecebeb"
+                        >
+                            <rect x="160" y="8" rx="3" ry="3" width="400" height="20" />
+                            <rect x="160" y="50" rx="3" ry="3" width="300" height="20" />
+                            <circle cx="50" cy="50" r="50" />
+                        </ContentLoader>
                     </div>
-                    <div className="col-9 box rounded pt-4 pb-3 pl-5 pr-2">
-                        <h2 className="h3">{userData.display_name}</h2>
-                        {observatoryData && (
-                            <p>
-                                <b className="d-block">Observatory</b>
-                                {observatoryData?.name}
-                            </p>
-                        )}
-                        {organizationData && (
-                            <p>
-                                <b className="d-block">Organization</b>
-                                {organizationData?.name}
-                            </p>
-                        )}
-                    </div>
-                </Row>
+                )}
             </Container>
 
             <Container className="d-flex align-items-center mt-4 mb-4">
