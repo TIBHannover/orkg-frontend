02d4dcf30686a91f64d7b2b8cf0d7b3dc46a4d09
Allard Oelen <oelen@l3s.de>
2021-03-16 12:20:04 +0100

    remove debug statements

--- src/components/ContributionEditor/__tests__/EditorTable.test.js
+++ src/components/ContributionEditor/__tests__/EditorTable.test.js
@@ -100,11 +100,8 @@ describe('resources', () => {
         await waitFor(() => screen.getByText(/create "updated resource"/i));
 
         fireEvent.click(screen.getByText(/create "updated resource"/i));
-        screen.debug(undefined, 30000);
 
         await waitFor(() => screen.getByRole('button', { name: /updated resource/i, hidden: true }, { timeout: 5000 }));
-        screen.debug(undefined, 30000);
-
         expect(screen.getByRole('button', { name: /updated resource/i, hidden: true })).toBeInTheDocument();
     });
 
