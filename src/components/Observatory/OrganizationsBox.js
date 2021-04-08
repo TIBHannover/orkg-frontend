176a6c11c33a39e25cc7c4338cb151e059d1f97e
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-04-08 17:51:09 +0200

    fix Observatory components

--- src/components/Observatory/OrganizationsBox.js
+++ src/components/Observatory/OrganizationsBox.js
@@ -24,7 +24,7 @@ const OrganizationsBox = ({ isLoadingOrganizations, organizationsList }) => {
                                                 paddingBottom: '11px'
                                             }}
                                         >
-                                            <Link to={reverse(ROUTES.ORGANIZATION, { id: organization.id })}>
+                                            <Link to={reverse(ROUTES.ORGANIZATION, { id: organization.display_id })}>
                                                 <img
                                                     style={{ marginTop: 12 }}
                                                     height="50"
@@ -44,7 +44,7 @@ const OrganizationsBox = ({ isLoadingOrganizations, organizationsList }) => {
                                                 textAlign: 'center'
                                             }}
                                         >
-                                            <Link to={reverse(ROUTES.ORGANIZATION, { id: organization.id })}>{organization.name}</Link>
+                                            <Link to={reverse(ROUTES.ORGANIZATION, { id: organization.display_id })}>{organization.name}</Link>
                                         </div>
                                     );
                                 }
