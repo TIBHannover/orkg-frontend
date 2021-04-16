5426c0a3b5105a3e302dbcda6a0058c23dfa7ae8
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-04-16 13:45:34 +0200

    Show "no label" if there is no resource/class/literal in the SB

--- src/components/StatementBrowser/ValueItem/ValueItemTemplate.js
+++ src/components/StatementBrowser/ValueItem/ValueItemTemplate.js
@@ -151,10 +151,10 @@ export default function ValueItemTemplate(props) {
                         <Button className="p-0 text-left" color="link" onClick={props.handleOnClick} style={{ userSelect: 'text' }}>
                             {props.showHelp && props.value.type === 'object' ? (
                                 <Pulse content="Click on the resource to browse it">
-                                    <ValuePlugins type="resource">{getLabel() || ''}</ValuePlugins>
+                                    <ValuePlugins type="resource">{getLabel() || <i>No label</i>}</ValuePlugins>
                                 </Pulse>
                             ) : (
-                                <ValuePlugins type="resource">{getLabel() || ''}</ValuePlugins>
+                                <ValuePlugins type="resource">{getLabel() || <i>No label</i>}</ValuePlugins>
                             )}
 
                             {props.resource && props.resource.existingResourceId && openExistingResourcesInDialog ? (
@@ -169,15 +169,15 @@ export default function ValueItemTemplate(props) {
                     )}
 
                     {props.resource && props.value.type === 'object' && !props.resource.isFetching && resourcesAsLinks && (
-                        <Link to={reverse(ROUTES.RESOURCE, { id: props.value.resourceId })}>{props.value.label}</Link>
+                        <Link to={reverse(ROUTES.RESOURCE, { id: props.value.resourceId })}>{props.value.label || <i>No label</i>}</Link>
                     )}
 
                     {!props.resource && props.value.type === 'class' && resourcesAsLinks && (
-                        <Link to={reverse(ROUTES.CLASS, { id: props.value.resourceId })}>{props.value.label}</Link>
+                        <Link to={reverse(ROUTES.CLASS, { id: props.value.resourceId })}>{props.value.label || <i>No label</i>}</Link>
                     )}
 
                     {!props.resource && props.value.type === 'predicate' && resourcesAsLinks && (
-                        <Link to={reverse(ROUTES.PROPERTY, { id: props.value.resourceId })}>{props.value.label}</Link>
+                        <Link to={reverse(ROUTES.PROPERTY, { id: props.value.resourceId })}>{props.value.label || <i>No label</i>}</Link>
                     )}
 
                     {props.resource && props.resource.isFetching && props.value.type === 'object' && 'Loading...'}
