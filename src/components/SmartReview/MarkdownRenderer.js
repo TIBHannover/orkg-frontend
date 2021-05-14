963446fd32a4a5f86c48fc86909b6d54da0d6903
Allard Oelen <oelen@l3s.de>
2021-05-14 11:21:04 +0200

    fix(SmartReview): underlined markdown text, add blockquote style
    Fixes: #588

--- src/components/SmartReview/MarkdownRenderer.js
+++ src/components/SmartReview/MarkdownRenderer.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import * as Showdown from 'showdown';
 import footnotes from 'showdown-footnotes';
+import styled from 'styled-components';
+
+const MarkdownContainer = styled.p`
+    blockquote {
+        color: rgba(0, 0, 0, 0.5);
+        padding-left: 1.5em;
+        border-left: 5px solid rgba(0, 0, 0, 0.1);
+    }
+`;
 
 const converter = new Showdown.Converter({
     openLinksInNewWindow: true,
-    extensions: [footnotes]
+    extensions: [footnotes],
+    underline: true
 });
 converter.setFlavor('github');
 
@@ -17,7 +27,7 @@ converter.setFlavor('github');
  */
 /* TODO: secure ADD_ATTR */
 const MarkdownRenderer = ({ text }) => {
-    return <p dangerouslySetInnerHTML={{ __html: sanitize(converter.makeHtml(text), { ADD_ATTR: ['target'] }) }} />;
+    return <MarkdownContainer dangerouslySetInnerHTML={{ __html: sanitize(converter.makeHtml(text), { ADD_ATTR: ['target'] }) }} />;
 };
 
 MarkdownRenderer.propTypes = {
