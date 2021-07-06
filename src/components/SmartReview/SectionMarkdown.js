78d4e70aa674bb438af5ba751bc373e7d4559c8e
Allard Oelen <oelen@l3s.de>
2021-07-06 10:29:30 +0200

    support entity linking, improve reference autocomplete

--- src/components/SmartReview/SectionMarkdown.js
+++ src/components/SmartReview/SectionMarkdown.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
-import { faBold, faCode, faImage, faItalic, faLink, faList, faListOl, faQuoteLeft, faUnderline } from '@fortawesome/free-solid-svg-icons';
+import { faBold, faCode, faImage, faItalic, faLink, faList, faListOl, faQuoteLeft, faTable, faUnderline } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react';
+import ReactTextareaAutocomplete from '@webscopeio/react-textarea-autocomplete';
+import '@webscopeio/react-textarea-autocomplete/style.css';
 import { updateSectionMarkdown } from 'actions/smartReview';
 import MarkdownRenderer from 'components/SmartReview/MarkdownRenderer';
 import { MarkdownPlaceholder } from 'components/SmartReview/styled';
@@ -9,11 +11,10 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Textarea from 'react-textarea-autosize';
-import { ButtonGroup } from 'reactstrap';
 import { toast } from 'react-toastify';
+import { ButtonGroup } from 'reactstrap';
+import { getResource, getResources } from 'services/backend/resources';
 import styled from 'styled-components';
-import ReactTextareaAutocomplete from '@webscopeio/react-textarea-autocomplete';
-import '@webscopeio/react-textarea-autocomplete/style.css';
 
 const Toolbar = styled.div`
     position: sticky;
@@ -33,9 +34,20 @@ const MarkdownSection = styled.div`
     }
 `;
 
-const Item = ({ entity: { name } }) => (
-    <div role="button" onMouseDown={e => e.preventDefault()}>
-        {name}
+const ItemReference = ({ entity: { reference } }) => (
+    <div role="button" onMouseDown={e => e.preventDefault()} className="px-2 py-1">
+        {reference.id}{' '}
+        <span className="font-italic ml-2" style={{ opacity: 0.7 }}>
+            {reference?.author?.[0]?.family ?? ''} {reference.author.length > 0 ? 'et al.' : ''} {reference?.issued?.['date-parts']?.[0] ?? ''}
+        </span>
+    </div>
+);
+const ItemResource = ({ entity: { resource } }) => (
+    <div role="button" onMouseDown={e => e.preventDefault()} className="px-2 py-1">
+        {resource.label}
+        <span className="font-italic ml-2" style={{ opacity: 0.7 }}>
+            {resource.id}
+        </span>
     </div>
 );
 const Loading = () => <div>Loading</div>;
@@ -105,6 +117,47 @@ const SectionMarkdown = props => {
         setSelectionRange({ start, end, length: openTag.length });
     };
 
+    const findResources = async token => {
+        if (token.startsWith('!#')) {
+            const resourceId = token.substring(2);
+            if (!resourceId) {
+                return [];
+            }
+            try {
+                const resource = await getResource(resourceId);
+                return [
+                    {
+                        resource
+                    }
+                ];
+            } catch (e) {
+                return [];
+            }
+        } else {
+            return token && token !== '!'
+                ? (await getResources({ q: token.substring(1), returnContent: true, items: 10 })).map(resource => ({
+                      resource
+                  }))
+                : [];
+        }
+    };
+    const autocompleteTriggers = {
+        '[!': {
+            dataProvider: findResources,
+            component: ItemResource,
+            allowWhitespace: true,
+            output: item => `[${item.resource.label}](https://www.orkg.org/orkg/resource/${item.resource.id})`
+        },
+        '[@': {
+            dataProvider: token =>
+                references
+                    .filter(reference => reference.parsedReference.id.toLowerCase().startsWith(token.toLowerCase().substring(1)))
+                    .map(reference => ({ reference: reference.parsedReference, char: reference.parsedReference.id })),
+            component: ItemReference,
+            output: item => `[@${item.char}]`
+        }
+    };
+
     return (
         <>
             {!editMode && (
@@ -172,7 +225,19 @@ const SectionMarkdown = props => {
                                     <Icon icon={faQuoteLeft} />
                                 </div>
                             </Tippy>
-                            <Tippy content="Add a code">
+                            <Tippy content="Add table">
+                                <div
+                                    role="button"
+                                    tabIndex="0"
+                                    className="btn btn-dark"
+                                    onMouseDown={e =>
+                                        wrapText(e, '| Header 1 | Header 2 |\n| ------ | ------ |\n| Item   | Item   |\n| Item   | Item   |')
+                                    }
+                                >
+                                    <Icon icon={faTable} />
+                                </div>
+                            </Tippy>
+                            <Tippy content="Add code">
                                 <div role="button" tabIndex="0" className="btn btn-dark" onMouseDown={e => wrapText(e, '`', '`')}>
                                     <Icon icon={faCode} />
                                 </div>
@@ -188,17 +253,8 @@ const SectionMarkdown = props => {
                             setMarkdownEditorRef(ref);
                         }}
                         dropdownStyle={{ zIndex: 100 }}
-                        minChar={1}
-                        trigger={{
-                            '[': {
-                                dataProvider: token =>
-                                    references
-                                        .filter(reference => reference.parsedReference.id.startsWith(token.substring(1)))
-                                        .map(reference => ({ name: reference.parsedReference.id, char: reference.parsedReference.id })),
-                                component: Item,
-                                output: item => `[@${item.char}]`
-                            }
-                        }}
+                        minChar={0}
+                        trigger={autocompleteTriggers}
                         onChange={e => {
                             setMarkdownValue(e.target.value);
                             if (e.target.value.length > 3899) {
