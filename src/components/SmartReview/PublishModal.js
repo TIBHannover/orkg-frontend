0731a5cd2cc33ac73bf9c1b821e93643165e9b0d
Allard Oelen <oelen@l3s.de>
2021-07-10 15:00:49 +0200

    feat(SmartReview): publish reviews with DOI

--- src/components/SmartReview/PublishModal.js
+++ src/components/SmartReview/PublishModal.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, CustomInput } from 'reactstrap';
 import { createLiteralStatement, createResourceStatement, getStatementsBundleBySubject } from 'services/backend/statements';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -13,15 +13,32 @@ import { reverse } from 'named-urls';
 import routes from 'constants/routes';
 import { Link } from 'react-router-dom';
 import { setVersions } from 'actions/smartReview';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Tooltip from 'components/Utils/Tooltip';
+import { generateDOIForComparison } from 'services/backend/misc';
+import ROUTES from 'constants/routes';
 
 const PublishModal = ({ id, show, toggle, getVersions, paperId }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [updateMessage, setUpdateMessage] = useState('');
     const [publishedId, setPublishedId] = useState(null);
+    const [shouldAssignDoi, setShouldAssignDoi] = useState(false);
+    const [description, setDescription] = useState('');
+    const { title } = useSelector(state => state.smartReview.paper);
+    const researchField = useSelector(state => state.smartReview.researchField);
+
     const dispatch = useDispatch();
 
+    const getUrl = () =>
+        `${window.location.protocol}//${window.location.host}${window.location.pathname
+            .replace(reverse(ROUTES.SMART_REVIEW, { id }), '')
+            .replace(/\/$/, '')}`;
+
     const handlePublish = async () => {
+        if (shouldAssignDoi && (!description || description.trim() === '')) {
+            toast.error('Please enter a description');
+            return;
+        }
         setIsLoading(true);
 
         try {
@@ -38,6 +55,30 @@ const PublishModal = ({ id, show, toggle, getVersions, paperId }) => {
                 resourceId: versionResource.id,
                 data: { rootResource: id, statements }
             });
+
+            if (shouldAssignDoi) {
+                // TODO: rename function, use destructuring for params
+                generateDOIForComparison(
+                    versionResource.id,
+                    title,
+                    researchField ? researchField.label : '',
+                    description,
+                    [], // TODO: add article authors
+                    [], // TODO: maybe add all comparison contributions?
+                    `${getUrl()}${reverse(ROUTES.SMART_REVIEW, { id: versionResource.id })}`
+                )
+                    .then(doiResponse => {
+                        const doiLiteral = createLiteral(doiResponse.data.attributes.doi);
+                        createResourceStatement(versionResource.id, PREDICATES.HAS_DOI, doiLiteral.id);
+                        setIsLoading(false);
+                        toast.success('DOI has been registered successfully');
+                    })
+                    .catch(error => {
+                        toast.error(`Error publishing a DOI`);
+                        console.log(error);
+                        setIsLoading(false);
+                    });
+            }
 
             // reload versions to ensure new versions appears in history
             const versions = await getVersions(paperId);
@@ -57,16 +98,49 @@ const PublishModal = ({ id, show, toggle, getVersions, paperId }) => {
             <ModalHeader toggle={toggle}>Publish article</ModalHeader>
             <ModalBody>
                 {!publishedId ? (
-                    <FormGroup>
-                        <Label for="update-message">Update message</Label>
-                        <Input
-                            type="text"
-                            id="update-message"
-                            placeholder="Example: added introduction section"
-                            value={updateMessage}
-                            onChange={e => setUpdateMessage(e.target.value)}
-                        />
-                    </FormGroup>
+                    <>
+                        <FormGroup>
+                            <Label for="update-message">Update message</Label>
+                            <Input
+                                type="text"
+                                id="update-message"
+                                placeholder="Example: added introduction section"
+                                value={updateMessage}
+                                onChange={e => setUpdateMessage(e.target.value)}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <div>
+                                <Tooltip message="Assign a DOI to the published version of this article">
+                                    <CustomInput
+                                        onChange={e => {
+                                            setShouldAssignDoi(e.target.checked);
+                                        }}
+                                        checked={shouldAssignDoi}
+                                        id="switchAssignDoi"
+                                        type="switch"
+                                        inline
+                                        label="Assign DOI to publication"
+                                        className="m-0"
+                                    />
+                                </Tooltip>
+                            </div>
+                        </FormGroup>
+                        {shouldAssignDoi && (
+                            <FormGroup>
+                                <Label for="description">
+                                    <Tooltip message="Briefly describe the contents of the article">Description</Tooltip>
+                                </Label>
+                                <Input
+                                    type="textarea"
+                                    name="description"
+                                    value={description}
+                                    id="description"
+                                    onChange={e => setDescription(e.target.value)}
+                                />
+                            </FormGroup>
+                        )}
+                    </>
                 ) : (
                     <Link to={reverse(routes.SMART_REVIEW, { id: publishedId })} onClick={toggle}>
                         View the published article
