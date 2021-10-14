0f3070b8bd01b5e16996256010053886fcb5cfcf
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-10-14 16:04:47 +0200

    Add support for current version of strapi

--- src/components/Layout/Header/AboutMenu.js
+++ src/components/Layout/Header/AboutMenu.js
@@ -1,35 +1,43 @@
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import ROUTES from 'constants/routes.js';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 import { DropdownItem, DropdownMenu, DropdownToggle, UncontrolledButtonDropdown } from 'reactstrap';
-import { getAboutPages } from 'services/cms';
+import { getAboutPagesMenu } from 'services/cms';
 import { reverseWithSlug } from 'utils';
+import { reverse } from 'named-urls';
 
 const AboutMenu = ({ closeMenu }) => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
         const getItems = async () => {
-            setItems(await getAboutPages());
+            setItems(await getAboutPagesMenu());
         };
         getItems();
     }, []);
 
     return (
         <UncontrolledButtonDropdown direction="right" className="w-100">
-            <DropdownToggle tag="button" className="dropdown-item w-100 pr-3">
-                About <FontAwesomeIcon style={{ marginTop: '4px' }} icon={faChevronRight} pull="right" />
+            <DropdownToggle
+                onClick={() => (items.length > 0 ? null : closeMenu())}
+                to={reverse(ROUTES.ABOUT, {})}
+                tag={items.length > 0 ? 'button' : RouterNavLink}
+                className="dropdown-item w-100 pr-3"
+            >
+                About {items.length > 0 && <Icon style={{ marginTop: '4px' }} icon={faChevronRight} pull="right" />}
             </DropdownToggle>
-            <DropdownMenu>
-                {items.map(({ id, title }) => (
-                    <DropdownItem tag={RouterNavLink} exact to={reverseWithSlug(ROUTES.ABOUT, { id, slug: title })} onClick={() => closeMenu()}>
-                        {title}
-                    </DropdownItem>
-                ))}
-            </DropdownMenu>
+            {items.length > 0 && (
+                <DropdownMenu>
+                    {items.map(({ id, title }) => (
+                        <DropdownItem tag={RouterNavLink} exact to={reverseWithSlug(ROUTES.ABOUT, { id, slug: title })} onClick={() => closeMenu()}>
+                            {title}
+                        </DropdownItem>
+                    ))}
+                </DropdownMenu>
+            )}
         </UncontrolledButtonDropdown>
     );
 };
