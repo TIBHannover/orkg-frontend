9de9e18939c0d463c35d5ec3c890c05a010d3ce0
Kheir Eddine Farfar <kfarfar@gmail.com>
2021-11-10 10:44:16 +0100

    create separated test file for formatted label and mock UserAvatar

--- src/components/StatementBrowser/__tests__/StatementBrowser.formattedLabel.js
+++ src/components/StatementBrowser/__tests__/StatementBrowser.formattedLabel.js
@@ -5,7 +5,7 @@ import { ENTITIES } from 'constants/graphSettings';
 jest.mock('react-flip-move', () => ({ children }) => children);
 jest.mock('components/UserAvatar/UserAvatar', () => () => null);
 
-const setup = (
+const setup = async (
     initialState = {},
     props = {
         initialSubjectId: 'R1',
@@ -17,16 +17,19 @@ const setup = (
     render(<StatementBrowser {...props} />, { initialState });
 };
 
-describe('statement browser', () => {
-    test('should render statement browser', async () => {
+describe('formatted label', () => {
+    test('should render Basic reproduction number contribution', async () => {
         const config = {
-            initialSubjectId: 'R1',
+            initialSubjectId: 'R44727',
             initialSubjectLabel: null,
             newStore: true,
             rootNodeType: ENTITIES.RESOURCE
         };
         setup({}, config);
         await waitFor(() => expect(screen.queryByText(/Loading/i)).toBeInTheDocument());
-        expect(await screen.findByText(/No data yet/i)).toBeInTheDocument();
+        expect(await screen.findByText(/3.1/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Lombardy, Italy/i)).toBeInTheDocument();
+        expect(await screen.findByText(/Determination of the COVID-19 basic reproduction number/i)).toBeInTheDocument();
+        expect(await screen.findByText(/2020-01-14 - 2020-03-08/i)).toBeInTheDocument();
     });
 });
