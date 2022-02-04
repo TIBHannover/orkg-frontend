bf587e953739c900d4635f08442797787631a852
Allard Oelen <oelen@l3s.de>
2022-02-04 15:57:00 +0100

    also set max-width for literature list markdown renderer

--- src/components/SmartReview/MarkdownRenderer.js
+++ src/components/SmartReview/MarkdownRenderer.js
@@ -5,26 +5,10 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Showdown from 'showdown';
 import footnotes from 'showdown-footnotes';
-import styled from 'styled-components';
 import REGEX from 'constants/regex';
 import Cite from 'citation-js';
 import showdownVideoPlugin from 'components/ArticleBuilder/MarkdownEditor/showdownVideoPlugin';
-
-const MarkdownContainer = styled.p`
-    blockquote {
-        color: rgba(0, 0, 0, 0.5);
-        padding-left: 1.5em;
-        border-left: 5px solid rgba(0, 0, 0, 0.1);
-    }
-    table td,
-    table th {
-        border: 1px solid #c4c4c4;
-        padding: 3px 5px;
-    }
-    img {
-        max-width: 100%;
-    }
-`;
+import { MarkdownContainer } from 'components/ArticleBuilder/MarkdownEditor/styled';
 
 /**
  *
