843a2c3c393241f12276df57385c1935e6872221
Allard Oelen <oelen@l3s.de>
2022-08-08 11:54:58 +0200

    use getParentResearchFields for determining field

--- src/components/AddPaper/EntityRecognition/useDetermineResearchField.js
+++ src/components/AddPaper/EntityRecognition/useDetermineResearchField.js
@@ -1,22 +1,22 @@
 import { RESOURCES } from 'constants/graphSettings';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { getStatementsBySubject } from 'services/backend/statements';
+import { getParentResearchFields } from 'services/backend/statements';
 
 const useDetermineResearchField = () => {
     const { selectedResearchField } = useSelector(state => state.addPaper);
     const [isComputerScienceField, setIsComputerScienceField] = useState(false);
 
     useEffect(() => {
-        if (selectedResearchField === RESOURCES.RESEARCH_FIELD_COMPUTER_SCIENCE) {
-            setIsComputerScienceField(true);
-            return;
-        }
-        const getComputerScienceSubFields = async () => {
-            const computerScienceSubFields = await getStatementsBySubject({ id: RESOURCES.RESEARCH_FIELD_COMPUTER_SCIENCE });
-            setIsComputerScienceField(computerScienceSubFields.some(statement => statement.object.id === selectedResearchField));
+        const determineField = async () => {
+            if (selectedResearchField === RESOURCES.RESEARCH_FIELD_COMPUTER_SCIENCE) {
+                setIsComputerScienceField(true);
+                return;
+            }
+            const parentFields = await getParentResearchFields(selectedResearchField);
+            setIsComputerScienceField(parentFields.some(field => field.id === RESOURCES.RESEARCH_FIELD_COMPUTER_SCIENCE));
         };
-        getComputerScienceSubFields();
+        determineField();
     }, [selectedResearchField]);
 
     return { isComputerScienceField };
