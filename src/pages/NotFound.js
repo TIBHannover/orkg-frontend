d2b549bd2dfdd669c8a656b09f7578a2cf728ccb
Kheir Eddine Farfar <kfarfar@gmail.com>
2022-08-26 18:48:21 +0200

    convert NotFound to functional component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Button, Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import ROUTES from 'constants/routes.js';
@@ -6,34 +6,31 @@ import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faBug } from '@fortawesome/free-solid-svg-icons';
 import TitleBar from 'components/TitleBar/TitleBar';
 
-class NotFound extends Component {
-    componentDidMount = () => {
+const NotFound = () => {
+    useEffect(() => {
         document.title = 'Page not found - ORKG';
-    };
-
-    render() {
-        return (
-            <div>
-                <TitleBar>An error has occurred</TitleBar>
-                <Container className="box rounded pt-4 pb-4 ps-5 pe-5">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-md-12 text-center">
-                                <span className="display-1 d-block">404</span>
-                                <Icon icon={faBug} className="text-primary mt-3 mb-3" style={{ fontSize: 25 }} />
-                                <div className="mb-4 lead">The page you are looking for was not found.</div>
-                                <Link to={ROUTES.HOME}>
-                                    <Button color="primary" className="me-3">
-                                        Back to home
-                                    </Button>
-                                </Link>
-                            </div>
+    }, []);
+    return (
+        <div>
+            <TitleBar>An error has occurred</TitleBar>
+            <Container className="box rounded pt-4 pb-4 ps-5 pe-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 text-center">
+                            <span className="display-1 d-block">404</span>
+                            <Icon icon={faBug} className="text-primary mt-3 mb-3" style={{ fontSize: 25 }} />
+                            <div className="mb-4 lead">The page you are looking for was not found.</div>
+                            <Link to={ROUTES.HOME}>
+                                <Button color="primary" className="me-3">
+                                    Back to home
+                                </Button>
+                            </Link>
                         </div>
                     </div>
-                </Container>
-            </div>
-        );
-    }
-}
+                </div>
+            </Container>
+        </div>
+    );
+};
 
 export default NotFound;
