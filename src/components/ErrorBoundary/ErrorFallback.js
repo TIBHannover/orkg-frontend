28a6e8db4d22951f71f384165b7b5d97bc9c3821
Kheir Eddine Farfar <kfarfar@gmail.com>
2022-08-26 18:49:13 +0200

    Add ErrorBoundary and ErrorFallback

---
+++ src/components/ErrorBoundary/ErrorFallback.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import { Button, Container, Collapse, Card, CardBody } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import ROUTES from 'constants/routes.js';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faBug } from '@fortawesome/free-solid-svg-icons';
+import { ReactComponent as Logo } from 'assets/img/logo.svg';
+import { detect } from 'detect-browser';
+import PropTypes from 'prop-types';
+
+const ErrorFallback = props => {
+    const [collapse, setCollapse] = useState(false);
+    const browser = detect();
+
+    useEffect(() => {
+        document.title = 'Something went wrong - ORKG';
+    }, []);
+
+    return (
+        <div>
+            <Container className="py-4 px-2">
+                <Logo />
+            </Container>
+
+            <Container className="box rounded pt-4 pb-4 ps-5 pe-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 text-center">
+                            <span className="display-6 d-block">Something went wrong!</span>
+                            <Icon icon={faBug} className="text-primary mt-3 mb-3" style={{ fontSize: 30 }} />
+                            <div className="mb-4 lead">
+                                We're quite sorry about this! Please try again or report an issue to help fix this problem
+                            </div>
+                            <Link to={ROUTES.HOME}>
+                                <Button color="primary" className="me-3 mb-1">
+                                    Back to home
+                                </Button>
+                            </Link>
+                            <a
+                                href={`https://gitlab.com/TIBHannover/orkg/orkg-frontend/-/issues/new?issue[title]=${
+                                    props.error
+                                }&issue[description]=%0A%0A%0A%23%23%23 Error details%0AError: ${props.error}%0A%0ALocation: ${
+                                    window.location.href
+                                }%0A%0ABrowser: ${JSON.stringify(browser)}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Button color="primary" outline className="me-3 mb-1">
+                                    Report an issue in Gitlab
+                                </Button>
+                            </a>
+                        </div>
+                        <div>
+                            <p
+                                role="button"
+                                onKeyPress={() => setCollapse(v => !v)}
+                                onClick={() => setCollapse(v => !v)}
+                                outline
+                                size="sm"
+                                className="d-inline-block text-small"
+                            >
+                                <small className="text-muted">&#8226; Show error details</small>
+                            </p>
+                        </div>
+                        <Collapse isOpen={collapse}>
+                            <Card>
+                                <CardBody>
+                                    <b>Error:</b> {props.error?.message}
+                                    <br />
+                                    <b>Location:</b> {window.location.href}
+                                    <br />
+                                    <b>Browser:</b> {JSON.stringify(browser)}
+                                </CardBody>
+                            </Card>
+                        </Collapse>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+ErrorFallback.propTypes = {
+    error: PropTypes.object,
+};
+
+export default ErrorFallback;
