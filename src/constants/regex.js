3e70ce2404555a82aa54da224e2b50815a95ab5d
Kheir Eddine Farfar <kfarfar@gmail.com>
2022-10-18 14:55:25 +0200

    fix: Incorrectly identified error message for Valid DOI

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -2,7 +2,7 @@ const regex = {
     // eslint-disable-next-line
     URL: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/i,
     // eslint-disable-next-line no-useless-escape
-    DOI: /^\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b$/i, // source: https://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
+    DOI: /^\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'])\S)+)\b$/i, // source: https://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page#comment24134610_10324802
     PERMALINK: /^[a-zA-Z0-9_]+$/, // used to validate the observatory and organization slug
     TIB_URL: /^(https?:)?\/\/av\.tib\.eu(\/(media|player)?(\?.*)?)\//,
     YOUTUBE_URL: /^(https?:)?\/\/(www.)?youtube\.com\/watch\?v=/,
