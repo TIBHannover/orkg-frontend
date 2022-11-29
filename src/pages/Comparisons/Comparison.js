3bd74ebd0a835294a6a7c79985682e84c52674ad
Allard Oelen <oelen@l3s.de>
2022-11-29 12:36:41 +0700

    feat(Comparison): improve horizontal scrolling by always showing scrollbar

--- src/pages/Comparisons/Comparison.js
+++ src/pages/Comparisons/Comparison.js
@@ -1,7 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Alert } from 'reactstrap';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
 import ComparisonLoadingComponent from 'components/Comparison/ComparisonLoadingComponent';
 import ComparisonTable from 'components/Comparison/Comparison';
 import ProvenanceBox from 'components/Comparison/ComparisonFooter/ProvenanceBox/ProvenanceBox';
@@ -16,8 +14,6 @@ import ComparisonHeaderMenu from 'components/Comparison/ComparisonHeader/Compari
 import AppliedFilters from 'components/Comparison/ComparisonHeader/AppliedFilters';
 import Outline from 'components/Comparison/Outline';
 import { useParams } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
-import env from '@beam-australia/react-env';
 import { useSelector } from 'react-redux';
 
 const Comparison = () => {
@@ -26,22 +22,8 @@ const Comparison = () => {
     const isFailedLoadingMetadata = useSelector(state => state.comparison.isFailedLoadingMetadata);
     const isLoadingResult = useSelector(state => state.comparison.isLoadingResult);
     const isFailedLoadingResult = useSelector(state => state.comparison.isFailedLoadingResult);
-
     const contributionsList = useSelector(state => state.comparison.configuration.contributionsList);
     const fullWidth = useSelector(state => state.comparison.configuration.fullWidth);
-
-    /** adding some additional state for meta data * */
-
-    const [cookies, setCookie] = useCookies();
-
-    const [hideScrollHint, setHideScrollHint] = useState(cookies.seenShiftMouseWheelScroll ? cookies.seenShiftMouseWheelScroll : false);
-
-    const onDismissShiftMouseWheelScroll = () => {
-        // dismiss function for the alert thingy!;
-        setCookie('seenShiftMouseWheelScroll', true, { path: env('PUBLIC_URL'), maxAge: 315360000 }); // << TEN YEARS
-        setHideScrollHint(true);
-    };
-
     const containerStyle = fullWidth ? { maxWidth: 'calc(100% - 100px)' } : {};
 
     useEffect(() => {
@@ -60,19 +42,6 @@ const Comparison = () => {
 
                 {!isFailedLoadingMetadata && !isFailedLoadingResult && (
                     <>
-                        {contributionsList.length > 3 && (
-                            <Alert className="mt-3" color="info" isOpen={!hideScrollHint} toggle={onDismissShiftMouseWheelScroll}>
-                                <Icon icon={faLightbulb} /> Use{' '}
-                                <b>
-                                    <i>Shift</i>
-                                </b>{' '}
-                                +{' '}
-                                <b>
-                                    <i>Mouse Wheel</i>
-                                </b>{' '}
-                                for horizontal scrolling in the table.
-                            </Alert>
-                        )}
                         <AppliedFilters />
 
                         {!isLoadingResult && contributionsList.length > 1 && (
