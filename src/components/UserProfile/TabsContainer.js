8579ae51e84f4c3595f49cb01e42caf595218fcf
Aftab <QuratulAin.Aftab@tib.eu>
2023-03-07 11:48:36 +0100

    removed containers

--- src/components/UserProfile/TabsContainer.js
+++ src/components/UserProfile/TabsContainer.js
@@ -1,11 +1,9 @@
-/* eslint-disable linebreak-style */
 import Tabs from 'components/Tabs/Tabs';
 import Items from 'components/UserProfile/Items';
 import { CLASSES } from 'constants/graphSettings';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container } from 'reactstrap';
 
 function TabsContainer({ currentUserId }) {
     const params = useParams();
@@ -21,54 +19,36 @@ function TabsContainer({ currentUserId }) {
     };
 
     return (
-        <Container className="mt-2 p-0">
-            <Tabs
-                destroyInactiveTabPane={true}
-                onChange={onTabChange}
-                activeKey={isActiveTab ?? '1'}
-                items={[
-                    {
-                        label: 'Comparisons',
-                        key: '1',
-                        children: (
-                            <Container className="p-0">
-                                <Items filterLabel="comparisons" filterClass={CLASSES.COMPARISON} userId={userId} />
-                            </Container>
-                        ),
-                    },
+        <Tabs
+            destroyInactiveTabPane={true}
+            onChange={onTabChange}
+            activeKey={isActiveTab ?? '1'}
+            items={[
+                {
+                    label: 'Comparisons',
+                    key: '1',
+                    children: <Items filterLabel="comparisons" filterClass={CLASSES.COMPARISON} userId={userId} />,
+                },
 
-                    {
-                        label: 'Papers',
-                        key: '2',
-                        children: (
-                            <Container className="p-0">
-                                <Items filterLabel="papers" filterClass={CLASSES.PAPER} userId={userId} showDelete={userId === currentUserId} />
-                            </Container>
-                        ),
-                    },
-                    {
-                        label: 'Templates',
-                        key: '3',
-                        children: (
-                            <Container className="p-0">
-                                <Items filterLabel="templates" filterClass={CLASSES.TEMPLATE} userId={userId} showDelete={false} />
-                            </Container>
-                        ),
-                    },
+                {
+                    label: 'Papers',
+                    key: '2',
+                    children: <Items filterLabel="papers" filterClass={CLASSES.PAPER} userId={userId} showDelete={userId === currentUserId} />,
+                },
+                {
+                    label: 'Templates',
+                    key: '3',
+                    children: <Items filterLabel="templates" filterClass={CLASSES.TEMPLATE} userId={userId} showDelete={false} />,
+                },
 
-                    {
-                        label: 'Reviews',
-                        key: '4',
+                {
+                    label: 'Reviews',
+                    key: '4',
 
-                        children: (
-                            <Container className="p-0">
-                                <Items filterLabel="reviews" filterClass={CLASSES.SMART_REVIEW_PUBLISHED} userId={userId} showDelete={false} />
-                            </Container>
-                        ),
-                    },
-                ]}
-            />
-        </Container>
+                    children: <Items filterLabel="reviews" filterClass={CLASSES.SMART_REVIEW_PUBLISHED} userId={userId} showDelete={false} />,
+                },
+            ]}
+        />
     );
 }
 
