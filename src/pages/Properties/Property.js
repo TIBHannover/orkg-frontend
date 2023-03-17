326772c76a8af92ea792481b76c05c48562824b9
Kheir Eddine Farfar <kfarfar@gmail.com>
2023-03-17 15:45:46 +0100

    feat: Delete property

--- src/pages/Properties/Property.js
+++ src/pages/Properties/Property.js
@@ -1,19 +1,21 @@
-import { useState, useEffect } from 'react';
-import { Container, Button } from 'reactstrap';
-import { getPredicate } from 'services/backend/predicates';
-import StatementBrowser from 'components/StatementBrowser/StatementBrowser';
-import InternalServerError from 'pages/InternalServerError';
-import RequireAuthentication from 'components/RequireAuthentication/RequireAuthentication';
-import NotFound from 'pages/NotFound';
+import { faPen, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useLocation, useParams } from 'react-router-dom';
-import PropertyStatements from 'components/PropertyStatements/PropertyStatements';
-import { ENTITIES } from 'constants/graphSettings';
-import TitleBar from 'components/TitleBar/TitleBar';
-import ItemMetadata from 'components/Search/ItemMetadata';
-import EditModeHeader from 'components/EditModeHeader/EditModeHeader';
 import EditableHeader from 'components/EditableHeader';
+import EditModeHeader from 'components/EditModeHeader/EditModeHeader';
+import useDeleteProperty from 'components/Property/hooks/useDeleteProperty';
+import PropertyStatements from 'components/Property/PropertyStatements/PropertyStatements';
+import RequireAuthentication from 'components/RequireAuthentication/RequireAuthentication';
+import ItemMetadata from 'components/Search/ItemMetadata';
+import StatementBrowser from 'components/StatementBrowser/StatementBrowser';
+import TitleBar from 'components/TitleBar/TitleBar';
+import { ENTITIES } from 'constants/graphSettings';
+import InternalServerError from 'pages/InternalServerError';
+import NotFound from 'pages/NotFound';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+import { getPredicate } from 'services/backend/predicates';
 
 function Property() {
     const location = useLocation();
@@ -23,6 +25,8 @@ function Property() {
     const [editMode, setEditMode] = useState(false);
     const params = useParams();
     const propertyId = params.id;
+    const isCurationAllowed = useSelector(state => state.auth.user?.isCurationAllowed);
+    const { deleteProperty } = useDeleteProperty({ propertyId, redirect: true });
 
     useEffect(() => {
         const findPredicate = async () => {
@@ -86,13 +90,26 @@ function Property() {
                                     )}
                                 </h3>
                             ) : (
-                                <EditableHeader
-                                    id={params.id}
-                                    value={property?.label}
-                                    onChange={handleHeaderChange}
-                                    entityType={ENTITIES.PREDICATE}
-                                    curatorsOnly={true}
-                                />
+                                <>
+                                    <EditableHeader
+                                        id={params.id}
+                                        value={property?.label}
+                                        onChange={handleHeaderChange}
+                                        entityType={ENTITIES.PREDICATE}
+                                        curatorsOnly={true}
+                                    />
+                                    {editMode && isCurationAllowed && (
+                                        <Button
+                                            color="danger"
+                                            size="sm"
+                                            className="mt-2 mb-3"
+                                            style={{ marginLeft: 'auto' }}
+                                            onClick={deleteProperty}
+                                        >
+                                            <Icon icon={faTrash} /> Delete property
+                                        </Button>
+                                    )}
+                                </>
                             )}
                             <ItemMetadata item={property} showCreatedAt={true} showCreatedBy={true} />
                             <hr />
