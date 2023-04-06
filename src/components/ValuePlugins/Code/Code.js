8aa6ac62f355db8dd702af0fb2fe81aea619cbd7
Allard Oelen <allard.oelen@gmail.com>
2023-04-06 12:32:35 +0000

    feat(StatementBrowser): support syntax highlighting for Github URLs

---
+++ src/components/ValuePlugins/Code/Code.js
@@ -0,0 +1,75 @@
+import CopyToClipboardButton from 'components/ValuePlugins/Code/CopyToClipboardButton';
+import { ENTITIES } from 'constants/graphSettings';
+import REGEX from 'constants/regex';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { renderToString } from 'react-dom/server';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import python from 'react-syntax-highlighter/dist/esm/languages/hljs/python';
+import r from 'react-syntax-highlighter/dist/esm/languages/hljs/r';
+import github from 'react-syntax-highlighter/dist/esm/styles/hljs/github';
+import { Button } from 'reactstrap';
+
+SyntaxHighlighter.registerLanguage('python', python);
+SyntaxHighlighter.registerLanguage('r', r);
+
+const SUPPORTED_LANGUAGES = [
+    { extension: 'py', language: 'python' },
+    { extension: 'R', language: 'r' },
+];
+
+const Code = ({ children, type }) => {
+    const [code, setCode] = useState('');
+    const [isShowAll, setIsShowAll] = useState(false);
+    const label = children;
+    const labelToText = renderToString(label);
+    const isGithubUrl = labelToText && type === ENTITIES.LITERAL ? labelToText.match(new RegExp(REGEX.GITHUB_CODE_URL)) : false;
+    const isSupportedLanguage =
+        labelToText && type === ENTITIES.LITERAL ? SUPPORTED_LANGUAGES.find(language => labelToText.endsWith(`.${language.extension}`)) : false;
+    const isCodeBlock = isGithubUrl && isSupportedLanguage;
+
+    useEffect(() => {
+        const fetchCode = async () => {
+            const _code = await fetch(labelToText);
+            setCode(await _code.text());
+        };
+        if (isCodeBlock) {
+            fetchCode();
+        }
+    }, [isCodeBlock, labelToText]);
+
+    if (!labelToText) {
+        return '';
+    }
+
+    const codeToShow = !isShowAll ? `${code.split('\n').slice(0, 5).join('\n')}\n${code.split('\n').length > 5 && '...'}` : code;
+
+    if (isCodeBlock) {
+        return (
+            <>
+                <CopyToClipboardButton code={code}>
+                    <SyntaxHighlighter
+                        className="m-0"
+                        language={SUPPORTED_LANGUAGES.find(language => labelToText.endsWith(`.${language.extension}`)?.language)}
+                        style={github}
+                    >
+                        {codeToShow}
+                    </SyntaxHighlighter>
+                </CopyToClipboardButton>
+                <div>
+                    <Button size="sm" color="link" className="p-0" onClick={() => setIsShowAll(v => !v)}>
+                        {isShowAll ? 'Show less' : 'Show all'}
+                    </Button>
+                </div>
+            </>
+        );
+    }
+    return label;
+};
+
+Code.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+    type: PropTypes.oneOf([ENTITIES.RESOURCE, ENTITIES.LITERAL]),
+};
+
+export default Code;
--- src/components/ValuePlugins/Latex/Latex.js
+++ src/components/ValuePlugins/Latex/Latex.js
@@ -1,9 +1,9 @@
-import { Component } from 'react';
+import { ENTITIES } from 'constants/graphSettings';
 import PropTypes from 'prop-types';
-import MathJax from 'react-mathjax-preview';
 import { renderToString } from 'react-dom/server';
-import styled from 'styled-components';
+import MathJax from 'react-mathjax-preview';
 import ReactStringReplace from 'react-string-replace';
+import styled from 'styled-components';
 
 const StyledMathJax = styled(MathJax)`
     display: inline;
@@ -16,55 +16,49 @@ const StyledMathJax = styled(MathJax)`
     }
 `;
 
-class Latex extends Component {
-    constructor(props) {
-        super(props);
-        const expression = /(\${2}.*\${2})/gm;
-        this.supportedValues = new RegExp(expression);
+const Latex = props => {
+    const expression = /(\${2}.*\${2})/gm;
+    const supportedValues = new RegExp(expression);
+    const label = props.children;
+    const labelToText = renderToString(label);
+
+    if (!labelToText) {
+        return '';
     }
 
-    render() {
-        const label = this.props.children;
-        const labelToText = renderToString(label);
-
-        if (!labelToText) {
-            return '';
-        }
-
-        if (this.props.type === 'literal' && labelToText.match(this.supportedValues)) {
-            return ReactStringReplace(labelToText, this.supportedValues, (match, i) => (
-                <StyledMathJax
-                    key={i}
-                    config={{
-                        jax: ['input/TeX', 'output/SVG'],
-                        showMathMenu: false,
-                        SVG: {
-                            useFontCache: false,
-                            useGlobalCache: false,
-                            linebreaks: { automatic: true, width: 'container' },
-                        },
-                        skipStartupTypeset: true,
-                        'HTML-CSS': {
-                            linebreaks: { automatic: true, width: 'container' },
-                        },
-                    }}
-                    onLoad={() => {
-                        if (!window.MathJax) {
-                            return;
-                        }
-                        window.MathJax.Hub.Queue(['Rerender', window.MathJax.Hub]);
-                    }}
-                    math={match}
-                />
-            ));
-        }
-        return label;
+    if (props.type === ENTITIES.LITERAL && labelToText.match(supportedValues)) {
+        return ReactStringReplace(labelToText, supportedValues, (match, i) => (
+            <StyledMathJax
+                key={i}
+                config={{
+                    jax: ['input/TeX', 'output/SVG'],
+                    showMathMenu: false,
+                    SVG: {
+                        useFontCache: false,
+                        useGlobalCache: false,
+                        linebreaks: { automatic: true, width: 'container' },
+                    },
+                    skipStartupTypeset: true,
+                    'HTML-CSS': {
+                        linebreaks: { automatic: true, width: 'container' },
+                    },
+                }}
+                onLoad={() => {
+                    if (!window.MathJax) {
+                        return;
+                    }
+                    window.MathJax.Hub.Queue(['Rerender', window.MathJax.Hub]);
+                }}
+                math={match}
+            />
+        ));
     }
-}
+    return label;
+};
 
 Latex.propTypes = {
     children: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
-    type: PropTypes.oneOf(['resource', 'literal']),
+    type: PropTypes.oneOf([ENTITIES.RESOURCE, ENTITIES.LITERAL]),
 };
 
 export default Latex;
