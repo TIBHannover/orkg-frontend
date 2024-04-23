0bf5cfa04e322b9f3399c90e6987666fa13e2e55
Allard Oelen <allard.oelen@gmail.com>
2024-04-23 15:10:10 +0000

    fix: storybook building

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,20 @@
-function injectEnv(definitions) {
-    const env = 'process.env';
+// function injectEnv(definitions) {
+//     const env = 'process.env';
 
-    if (!definitions[env]) {
-        return {
-            ...definitions,
-            [env]: JSON.stringify(
-                Object.fromEntries(
-                    Object.entries(definitions)
-                        .filter(([key]) => key.startsWith(env))
-                        .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
-                ),
-            ),
-        };
-    }
-    return definitions;
-}
+//     if (!definitions[env]) {
+//         return {
+//             ...definitions,
+//             [env]: JSON.stringify(
+//                 Object.fromEntries(
+//                     Object.entries(definitions)
+//                         .filter(([key]) => key.startsWith(env))
+//                         .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
+//                 ),
+//             ),
+//         };
+//     }
+//     return definitions;
+// }
 
 module.exports = {
     stories: ['./**/*.stories.mdx', '../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -37,12 +37,23 @@ module.exports = {
         defaultName: 'Docs',
     },
     // fixes an issue where process.env is empty: https://github.com/storybookjs/storybook/issues/17336
-    webpackFinal: async config => {
-        const definePlugin = config.plugins.find(({ constructor }) => constructor && constructor.name === 'DefinePlugin');
-        if (definePlugin) {
-            definePlugin.definitions = injectEnv(definePlugin.definitions);
-        }
+    // webpackFinal: async (config) => {
+    //     const definePlugin = config.plugins.find(({ constructor }) => constructor && constructor.name === 'DefinePlugin');
+    //     if (definePlugin) {
+    //         definePlugin.definitions = injectEnv(definePlugin.definitions);
+    //     }
 
-        return config;
+    //     return config;
+    // },
+    // fixes an issue with nextjs, can be removed when updating to storybook 8 https://github.com/storybookjs/storybook/issues/25649
+    webpack(baseConfig) {
+        baseConfig.resolve = {
+            ...(baseConfig.resolve ?? {}),
+            alias: {
+                ...(baseConfig.resolve?.alias ?? {}),
+                '@opentelemetry/api': 'next/dist/compiled/@opentelemetry/api',
+            },
+        };
+        return baseConfig;
     },
 };
