f0edc5cc4d83318b8fb2dfb858f044fde09f5882
Kheir Eddine Farfar <kfarfar@gmail.com>
2024-05-03 18:46:06 +0200

    Enable environment variables in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,21 @@
-// function injectEnv(definitions) {
-//     const env = 'process.env';
+/**
+ * Gets a list of environment variables that start with `NEXT_PUBLIC_`.
+ */
+export function getPublicEnv(config) {
+    const publicEnv = Object.keys(JSON.parse(config))
+        .filter((key) => /^NEXT_PUBLIC_/i.test(key))
+        .reduce(
+            (env, key) => ({
+                ...env,
+                [key]: process.env[key],
+            }),
+            {},
+        );
 
-//     if (!definitions[env]) {
-//         return {
-//             ...definitions,
-//             [env]: JSON.stringify(
-//                 Object.fromEntries(
-//                     Object.entries(definitions)
-//                         .filter(([key]) => key.startsWith(env))
-//                         .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
-//                 ),
-//             ),
-//         };
-//     }
-//     return definitions;
-// }
+    return JSON.stringify(publicEnv);
+}
 
-module.exports = {
+const config = {
     stories: ['./**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
     framework: {
@@ -30,16 +29,10 @@ module.exports = {
         autodocs: true,
         defaultName: 'Docs',
     },
-    // fixes an issue where process.env is empty: https://github.com/storybookjs/storybook/issues/17336
-    // webpackFinal: async (config) => {
-    //     const definePlugin = config.plugins.find(({ constructor }) => constructor && constructor.name === 'DefinePlugin');
-    //     if (definePlugin) {
-    //         definePlugin.definitions = injectEnv(definePlugin.definitions);
-    //     }
-
-    //     return config;
-    // },
-    // fixes an issue with nextjs, can be removed when updating to storybook 8 https://github.com/storybookjs/storybook/issues/25649
+    env: (config) => ({
+        ...config,
+        STORYBOOK_ENVS: getPublicEnv(JSON.stringify(config)),
+    }),
     webpack(baseConfig) {
         baseConfig.resolve = {
             ...(baseConfig.resolve ?? {}),
@@ -52,3 +45,5 @@ module.exports = {
         return baseConfig;
     },
 };
+
+export default config;
