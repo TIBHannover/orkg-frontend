fdc90102af59039bab3774a77afbb1dbbd93105f
Kheir Eddine Farfar <kfarfar@gmail.com>
2024-12-18 16:53:25 +0100

    fix: remove document title update from the InternalServerError component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,41 +1,34 @@
 'use client';
 
-import Link from 'next/link';
-import { Component } from 'react';
-import { Button, Container } from 'reactstrap';
-import ROUTES from 'constants/routes';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBug } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TitleBar from 'components/TitleBar/TitleBar';
+import ROUTES from 'constants/routes';
+import Link from 'next/link';
+import { Button, Container } from 'reactstrap';
 
-class InternalServerError extends Component {
-    componentDidMount() {
-        document.title = 'Internal Server Error - ORKG';
-    }
-
-    render() {
-        return (
-            <div>
-                <TitleBar>An error has occurred</TitleBar>
-                <Container className="box rounded pt-4 pb-4 ps-5 pe-5">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-md-12 text-center">
-                                <span className="display-1 d-block">500</span>
-                                <FontAwesomeIcon icon={faBug} className="text-primary mt-3 mb-3" style={{ fontSize: 25 }} />
-                                <div className="mb-4 lead">Internal Server Error.</div>
-                                <Link href={ROUTES.HOME}>
-                                    <Button color="primary" className="me-3">
-                                        Back to home
-                                    </Button>
-                                </Link>
-                            </div>
+const InternalServerError = () => {
+    return (
+        <div>
+            <TitleBar>An error has occurred</TitleBar>
+            <Container className="box rounded pt-4 pb-4 ps-5 pe-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 text-center">
+                            <span className="display-1 d-block">500</span>
+                            <FontAwesomeIcon icon={faBug} className="text-primary mt-3 mb-3" style={{ fontSize: 25 }} />
+                            <div className="mb-4 lead">Internal Server Error.</div>
+                            <Link href={ROUTES.HOME}>
+                                <Button color="primary" className="me-3">
+                                    Back to home
+                                </Button>
+                            </Link>
                         </div>
                     </div>
-                </Container>
-            </div>
-        );
-    }
-}
+                </div>
+            </Container>
+        </div>
+    );
+};
 
 export default InternalServerError;
