71ecdd7235a67ae39e1fb8c344e67bb663e18707
AOelen <4102125-aoelen@users.noreply.gitlab.com>
2024-12-20 10:34:53 +0000

    feat(Search): add ORKG Ask example questions

--- src/components/Search/OrkgAskBanner/OrkgAskBanner.tsx
+++ src/components/Search/OrkgAskBanner/OrkgAskBanner.tsx
@@ -1,10 +1,41 @@
 import Image from 'next/image';
 import logo from 'components/Search/OrkgAskBanner/logo.png';
+import useSWR from 'swr';
+import { getLlmResponse, nlpServiceUrl } from 'services/orkgNlp';
+import { FC } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-const OrkgAskBanner = () => {
+type OrkgAskBannerProps = {
+    searchQuery?: string;
+};
+
+const OrkgAskBanner: FC<OrkgAskBannerProps> = () => {
+    const searchParams = useSearchParams();
+    const searchQuery = searchParams.get('q') || '';
+
+    const { data } = useSWR(
+        searchQuery
+            ? [
+                  {
+                      taskName: 'generateResearchQuestions',
+                      placeholders: { keywords: searchQuery },
+                  },
+                  nlpServiceUrl,
+                  'getLlmResponse',
+              ]
+            : null,
+        ([params]) => getLlmResponse(params),
+    );
+
     return (
-        <a href="https://ask.orkg.org" target="_blank" rel="noreferrer" className="text-body" style={{ fontSize: '95%' }}>
-            <div className="bg-light rounded px-3 py-2 d-flex align-items-center">
+        <div className="bg-light rounded p-3 text-body" style={{ fontSize: '95%' }}>
+            <a
+                href="https://ask.orkg.org"
+                target="_blank"
+                rel="noreferrer"
+                className="text-body d-flex align-items-center"
+                style={{ fontSize: '95%' }}
+            >
                 <div>
                     <Image src={logo} width="150" alt="Logo of ORKG Ask" />
                 </div>
@@ -12,8 +43,29 @@ const OrkgAskBanner = () => {
                     <div className="fw-bold">Search in ORKG Ask</div>
                     Find research you are actually looking for
                 </div>
-            </div>
-        </a>
+            </a>
+
+            {data && data?.questions?.length > 0 && (
+                <>
+                    <hr />
+                    <strong>Related questions</strong>
+                    <ul className="p-0 list-unstyled fst-italic">
+                        {data.questions.slice(0, 2).map((question: string) => (
+                            <li key={question} className="mt-1">
+                                <a
+                                    href={`https://ask.orkg.org/search?query=${encodeURIComponent(question)}`}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="text-body d-flex align-items-center"
+                                >
+                                    {question}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </div>
     );
 };
 
