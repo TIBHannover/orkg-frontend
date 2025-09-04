5822e57f10b44cf23863f02005291c650cdb9453
Kheir Eddine Farfar <kfarfar@gmail.com>
2025-09-04 15:59:07 +0200

    refactor: list only papers on the research problem page

--- src/components/ResearchProblem/ResearchProblemTabsContainer.tsx
+++ src/components/ResearchProblem/ResearchProblemTabsContainer.tsx
@@ -1,91 +1,51 @@
-import { useQueryState } from 'nuqs';
-
-import CardFactory from '@/components/Cards/CardFactory/CardFactory';
-import ContentTypeListHeader from '@/components/ContentTypeList/ContentTypeListHeader';
-import usePaginate from '@/components/PaginatedContent/hooks/usePaginate';
-import ListPaginatedContent from '@/components/PaginatedContent/ListPaginatedContent';
-import Tabs from '@/components/Tabs/Tabs';
-import { VISIBILITY_FILTERS } from '@/constants/contentTypes';
+import PaperCard from '@/components/Cards/PaperCard/PaperCard';
+import ListPage from '@/components/PaginatedContent/ListPage';
 import { CLASSES } from '@/constants/graphSettings';
-import { getResearchProblemContent, problemsUrl } from '@/services/backend/problems';
-import { Item, VisibilityOptions } from '@/services/backend/types';
+import { getPaper, getPapersLinkedToResource, papersUrl } from '@/services/backend/papers';
+import { PaginatedResponse, PaginationParams, Paper, Resource } from '@/services/backend/types';
 
 export const RESEARCH_PROBLEM_CONTENT_TABS = [
     { id: CLASSES.PAPER, label: 'Papers' },
-    { id: CLASSES.COMPARISON, label: 'Comparisons' },
-    { id: CLASSES.VISUALIZATION, label: 'Visualizations' },
+    // { id: CLASSES.COMPARISON, label: 'Comparisons' },
+    // { id: CLASSES.VISUALIZATION, label: 'Visualizations' },
     // { id: CLASSES.SMART_REVIEW_PUBLISHED, label: 'Reviews' },
     // { id: CLASSES.LITERATURE_LIST_PUBLISHED, label: 'Lists' },
 ];
 
 function ResearchProblemTabsContainer({ id }: { id: string }) {
-    const [contentType, setContentType] = useQueryState('contentType', { defaultValue: CLASSES.PAPER });
-    const [sort] = useQueryState<VisibilityOptions>('sort', {
-        defaultValue: VISIBILITY_FILTERS.TOP_RECENT,
-        parse: (value) => value as VisibilityOptions,
-    });
-
-    const renderListItem = (item: Item) => <CardFactory showBadge showCurationFlags showAddToComparison key={`item${item.id}`} item={item} />;
-
-    const {
-        data: items,
-        isLoading,
-        totalElements,
-        page,
-        hasNextPage,
-        totalPages,
-        error,
-        pageSize,
-        setPage,
-        setPageSize,
-    } = usePaginate({
-        fetchFunction: getResearchProblemContent,
-        fetchUrl: problemsUrl,
-        fetchFunctionName: 'getResearchProblemContent',
-        fetchExtraParams: {
-            id,
-            classes: [contentType],
-            visibility: sort,
-        },
-    });
-
-    const onTabChange = (tab: string) => {
-        setContentType(tab, { scroll: false, history: 'push' });
-        setPage(0);
+    const renderListItem = (object: Paper & { path: Resource[][] }) => {
+        return <PaperCard paper={object} key={object.id} />;
     };
 
+    const fetchItems = async (
+        params: {
+            id: string;
+            returnContent?: boolean;
+        } & PaginationParams,
+    ) => {
+        const result = await getPapersLinkedToResource(params);
+        const papers = await Promise.all((result as PaginatedResponse<Resource & { path: Resource[][] }>).content.map((p) => getPaper(p.id)));
+        return {
+            ...result,
+            content: papers.map((p) => ({
+                ...p,
+                path: (result as PaginatedResponse<Resource & { path: Resource[][] }>).content.find((rp) => rp.id === p.id)?.path,
+            })),
+        };
+    };
     return (
-        <>
-            <ContentTypeListHeader isLoading={isLoading} totalElements={totalElements} showSubFieldsFilter={false} />
-
-            <Tabs
-                className="box rounded mt-2"
-                destroyOnHidden
-                onChange={onTabChange}
-                activeKey={contentType}
-                items={RESEARCH_PROBLEM_CONTENT_TABS.map((tab) => ({
-                    label: tab.label,
-                    key: tab.id,
-                    children: (
-                        <ListPaginatedContent<Item>
-                            renderListItem={renderListItem}
-                            pageSize={pageSize}
-                            label="Research Problem"
-                            isLoading={isLoading}
-                            items={items ?? []}
-                            hasNextPage={hasNextPage}
-                            page={page}
-                            setPage={setPage}
-                            setPageSize={setPageSize}
-                            totalElements={totalElements}
-                            error={error}
-                            totalPages={totalPages}
-                            boxShadow={false}
-                        />
-                    ),
-                }))}
-            />
-        </>
+        <ListPage
+            label="papers"
+            boxShadow
+            renderListItem={renderListItem}
+            // @ts-ignore
+            fetchFunction={fetchItems}
+            fetchUrl={papersUrl}
+            fetchExtraParams={{ id, returnContent: false }}
+            disableSearch
+            defaultSortBy="paper.created_at"
+            flush
+        />
     );
 }
 
